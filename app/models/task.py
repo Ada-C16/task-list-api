848af43c5dad65b7f86ecc228f33678622d9da54
kaitlyngore <kaitlyngore@gmail.com>
2021-10-27 17:51:12 -0400

    update Task model, routes

--- app/models/task.py
+++ app/models/task.py
@@ -4,3 +4,18 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+    def create_dict(self):
+        complete_status = False
+        if self.completed_at:
+            complete_status = True
+
+        return {
+                        "id": self.task_id,
+                        "title": self.title,
+                        "description": self.description,
+                        "is_complete": complete_status,
+                    }
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, request
+from app.models.task import Task
+from app import db
 
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["GET", "POST"])
+def handle_tasks():
+    pass
