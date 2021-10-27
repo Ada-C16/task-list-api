1eb3e4324782e4f7ffca898acfcfc383c91ac91c
Lux Barker <lux.barker13@gmail.com>
2021-10-27 14:30:32 -0700

    implement get all tasks endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response
+from app import db
+from app.models.task import Task
 
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+
+@task_bp.route("", methods=["GET"])
+def read_tasks():
+    tasks = Task.query.all()
+    tasks = [task.to_dict() for task in tasks]
+    return jsonify(tasks), 200
