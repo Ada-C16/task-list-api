49cc1a8e284b31386762634437c73402065d39fd
Kbhlee2121 <Kbhlee2121@gmail.com>
2021-10-27 14:38:11 -0700

    Register task blueprint

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, request, abort
+from app import db
+from app.models.task import Task
+
+task_bp = Blueprint("task", __name__, url_prefix="/tasks")
+
 
