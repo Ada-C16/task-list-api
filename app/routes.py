18c068c81d03feba9e87277dc5491521e06cfbd8
Stephanie <stephanie.chenn1@gmail.com>
2021-10-27 17:26:15 -0400

    passed first two tests in wave 1

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app import db
+from app.models.task import Task
 
+# Create tasks blueprint
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["GET"])
+def handle_tasks():
+    tasks = Task.query.all()
+    tasks_response = []
+
+    for task in tasks:
+        tasks_response.append({
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": False
+        })
+    return jsonify(tasks_response)
