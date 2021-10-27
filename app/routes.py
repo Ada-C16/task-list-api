3a0f0caba4a27d3c63444130d6230aa2dbfb8593
Lia Gaetano <lgaetano19@gmail.com>
2021-10-27 15:36:59 -0700

    Moves Task into correct Models folder

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, abort, jsonify, request
+# from app.models.task import Task
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 
