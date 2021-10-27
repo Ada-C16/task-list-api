8faf6ca720d28bcfd6a648542dbbe927870c2ba9
Laurel S <ls60@protonmail.com>
2021-10-27 14:28:19 -0700

    Add endpoint to get all tasks with GET at /tasks

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,11 @@
-from flask import Blueprint, jsonify
+from app import db
+from flask import Blueprint, jsonify, make_response
+from app.models.task import Task
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["GET"], strict_slashes=False)
+def get_all_tasks():
+    all_tasks = Task.query.all()
+    tasks_response = [task.to_dict for task in all_tasks]
+    return make_response(jsonify(tasks_response), 200)
