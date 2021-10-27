65c82f59f043e32bf2512e91e8c011bb6758d06c
Monica Cruz <mcatcruz@gmail.com>
2021-10-27 10:31:22 -1000

    updated Task

--- app/models/task.py
+++ app/models/task.py
@@ -4,3 +4,6 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    task_title = db.Column(db.String)
+    task_description = db.Column(db.String)
+    task_completed_at = db.Column(db.DateTime, nullable=True)
