840b29bdc966b05be45e4cb92ce6c9532dd2c4f6
Kbhlee2121 <Kbhlee2121@gmail.com>
2021-10-27 14:56:51 -0700

    Create routes for adding a new task and for reading all tasks

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,37 @@
+import re
 from flask import Blueprint, jsonify, make_response, request, abort
+from werkzeug.exceptions import RequestEntityTooLarge
 from app import db
 from app.models.task import Task
 
 task_bp = Blueprint("task", __name__, url_prefix="/tasks")
 
+# Create a task
+@task_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    if "title" not in request_body or "description" not in request_body or "completed_at" not in request_body:
+        return {"error": "incomplete request body"}, 400
 
+    new_task = Task(
+        title = request_body["title"],
+        description = request_body["description"],
+        completed_at = request_body["completed_at"],
+    )
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response("New task created!", 201)
+
+# Read all tasks
+@task_bp.route("", methods=["GET"])
+def read_all_tasks():
+    tasks = Task.query.all()
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append(
+            task.to_dict()
+        )
+
+    return jsonify(tasks_response)
