4e6b0854b8add12c4fd57074ac52c461ec93ca84
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-10-27 16:10:36 -0700

    update Task class with more attributes

--- app/models/task.py
+++ app/models/task.py
@@ -3,4 +3,11 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    completed_at = db.Column(db.DateTime, nullable=True)
+    
+    priority = db.Column(db.Integer, nullable=False)
+    due_date = db.Column(db.DateTime, nullable=True)
+    created_date = db.Column(db.DateTime, default=db.func.current_timestamp())
