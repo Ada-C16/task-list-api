f8cc3ce84bc07eb2aed6a02fe8bc3f3b496a2690
Laurel S <ls60@protonmail.com>
2021-10-27 20:59:24 -0700

    Passes Wave 6 tests.

--- app/models/task.py
+++ app/models/task.py
@@ -6,6 +6,8 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'), nullable=True)
+    goal = db.relationship("Goal", back_populates="tasks")
     # completed_at = db.Column(db.DateTime, nullable=True, default=None)
     # # null in postman
     # is_complete = db.Column(db.Boolean, default=False)
@@ -15,9 +17,31 @@ class Task(db.Model):
             is_complete = True
         else:
             is_complete = False
-        return {
+        if self.goal_id:
+            return {
             "id": self.id,
             "title": self.title,
             "description": self.description,
-            "is_complete": is_complete
+            "is_complete": is_complete,
+            "goal_id": self.goal_id
         }
+        else:
+            return {
+                "id": self.id,
+                "title": self.title,
+                "description": self.description,
+                "is_complete": is_complete,
+            }
+    
+    # def to_dict_with_goal_id(self):
+    #     if self.completed_at:
+    #         is_complete = True
+    #     else:
+    #         is_complete = False
+    #     return {
+    #         "id": self.id,
+    #         "title": self.title,
+    #         "description": self.description,
+    #         "is_complete": is_complete,
+    #         "goal_id": self.goal_id
+    #     }
--- app/routes.py
+++ app/routes.py
@@ -136,3 +136,35 @@ def delete_goal(goal_id):
     db.session.commit()
     return make_response(
         {"details": f'Goal {selected_goal.id} "{selected_goal.title}" successfully deleted'}, 200)
+
+
+# goals and tasks routes
+@goals_bp.route("/<goal_id>/tasks", methods=["POST"], strict_slashes=False)
+def assign_tasks_to_goal(goal_id):
+    selected_goal = get_goal_from_id(goal_id)
+    request_body = request.get_json()
+    task_ids = request_body["task_ids"]
+    for task_id in task_ids:
+        selected_task = get_task_from_id(task_id)
+        selected_goal.tasks.append(selected_task)
+    db.session.add(selected_goal)
+    db.session.commit()
+    return make_response({
+            "id": selected_goal.id,
+            "task_ids": task_ids
+        }, 200)
+
+@goals_bp.route("/<goal_id>/tasks", methods=["GET"], strict_slashes=False)
+def get_tasks_belonging_to_goal(goal_id):
+    selected_goal = get_goal_from_id(goal_id)
+    # tasks is a list of dictionaries
+    tasks = selected_goal.tasks
+    response_tasks = []
+    for task in tasks:
+        response_tasks.append(task.to_dict())             
+    response = {
+    "id": selected_goal.id,
+    "title": selected_goal.title,
+    "tasks": response_tasks
+    }
+    return make_response(response, 200)
