345d0674d4b548ad8bd483dbe6b76a403ca9998b
Monica Cruz <mcatcruz@gmail.com>
2021-10-28 13:22:53 -1000

    Wave 1: 6 of 12 tests passing.

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, request
+from app import db
+from app.models.task import Task
 
+tasks_bp = Blueprint('tasks', __name__, url_prefix='/tasks')
+
+@tasks_bp.route('', methods=['GET', 'POST'])
+def handle_tasks():
+        if request.method == 'GET':
+            tasks_list = []
+            tasks = Task.query.all()
+            for task in tasks:
+                tasks_list.append({
+                    "id": task.task_id,
+                    "title": task.title,
+                    "description": task.description,
+                    "is_complete": False if task.completed_at is None else task.completed_at
+                })
+            return jsonify(tasks_list)
+            
+        elif request.method == 'POST':
+            request_body = request.get_json()
+            new_task = Task(title=request_body['title'],
+                            description=request_body['description'],
+                            completed_at=request_body['completed_at']
+                            )
+            db.session.add(new_task)
+            db.session.commit()
+            return make_response({"task": { "id": new_task.task_id,
+                                            "title": new_task.title,
+                                            "description": new_task.description,
+                                            "is_complete": False if new_task.completed_at is None else new_task.completed_at  
+                                            }}, 201)
