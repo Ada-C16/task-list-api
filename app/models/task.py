cbd43a205babb9865bbb5ed6c9fb96d931c4c601
kaitlyngore <kaitlyngore@gmail.com>
2021-10-28 16:33:32 -0400

    Add GET and POST methods

--- app/models/task.py
+++ app/models/task.py
@@ -4,19 +4,18 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    description = db.Column(db.String)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True)
 
     def create_dict(self):
-        complete_status = False
-        if self.completed_at:
-            complete_status = True
+        complete_status = True if self.completed_at else False
+        # if self.completed_at:
+        #     complete_status = True
 
-        return {"task":
-                    {
+        return {
                 "id": self.task_id,
                 "title": self.title,
                 "description": self.description,
                 "is_complete": complete_status,
-        }}
+        }
--- app/routes.py
+++ app/routes.py
@@ -8,11 +8,42 @@ tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 def handle_tasks():
     if request.method == "POST":
         request_body = request.get_json()
-        new_task = Task(title=request_body["title"],
-                            description=request_body["description"],
-                            )
+        if "title" not in request_body or "description" not in request_body:
+            error_dict = {"details": "Invalid data"}
+            return jsonify(error_dict), 400
+        
+        if "completed_at" in request_body:
+            new_task = Task(title=request_body["title"],
+                                description=request_body["description"],
+                                completed_at=request_body["completed_at"]
+                                )
+        else:
+            new_task = Task(title=request_body["title"],
+                            description=request_body["description"])
 
         db.session.add(new_task)
         db.session.commit()
 
-        return jsonify(new_task.create_dict()), 201
+        task_response = {"task": new_task.create_dict}
+        return jsonify(task_response), 201
+
+    elif request.method == "GET":
+        name_from_url = request.args.get("name")
+        if name_from_url:
+            tasks = Task.query.filter_by(name=name_from_url).all()
+            if not tasks:
+                tasks = Task.query.filter(Task.name.contains(name_from_url))
+                
+        else:
+            tasks = Task.query.all()
+            
+        tasks_response = []
+        for task in tasks:
+            tasks_response.append(task.create_dict())
+        
+        if not tasks_response:
+            tasks = Task.query.all()
+            for task in tasks:
+                tasks_response.append(task.create_dict())
+        
+        return jsonify(tasks_response)
