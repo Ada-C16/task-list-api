ebf2a8be76a6d66943a1c4101854c6690bb103b4
Michelle Bodart <michelle.bodart@gmail.com>
2021-10-28 08:49:51 -0700

    defines Task model and its columns

--- app/models/task.py
+++ app/models/task.py
@@ -3,4 +3,8 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False) # Not sure if I need to specify if these are nullable
+    description = db.Column(db.String, nullable=False)
+    completed_at = db.Column(db.DateTime, nullable=True)
+    is_complete = db.Column(db.String, nullable=True)
