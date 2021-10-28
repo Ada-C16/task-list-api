359f8beb8e3e8b6bfdaba30b8ceca02d8f1a0c7e
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-10-27 17:48:38 -0700

    alter Task class to match tests

--- app/models/task.py
+++ app/models/task.py
@@ -6,8 +6,22 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200), nullable=False)
-    completed_at = db.Column(db.DateTime, nullable=True)
     
-    priority = db.Column(db.Integer, nullable=False)
-    due_date = db.Column(db.DateTime, nullable=True)
+    # personalized attributes
+    #priority = db.Column(db.Integer, nullable=False)
+    #due_date = db.Column(db.DateTime, nullable=True)
+
+    # attributes with defaults
     created_date = db.Column(db.DateTime, default=db.func.current_timestamp())
+    completed_at = db.Column(db.DateTime, default=None)
+    is_complete = db.Column(db.Boolean, default=False)
+
+    def to_dict(self):
+        return {
+            'id': self.task_id,
+            'title': self.title,
+            'description': self.description,
+            #'completed_at': self.completed_at,
+            #'created_date': self.created_date,
+            'is_complete': self.is_complete
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,44 @@
-from flask import Blueprint
+from re import L
+from flask import Blueprint, jsonify, request, make_response
+from app.models.task import Task
+from app import db
 
+tasks_bp = Blueprint('tasks', __name__, url_prefix='/tasks')
+
+@tasks_bp.route('', methods=['GET', 'POST'])
+def handle_tasks():
+    if request.method == 'GET':
+        tasks = Task.query.all()
+        # if not tasks:
+        #     return make_response(jsonify({'message': 'No tasks found'}), 404)
+        return jsonify([task.to_dict() for task in tasks]), 200
+
+    elif request.method == 'POST':
+        request_body = request.get_json()
+        if isinstance(request_body, list):
+            for task in request_body:
+                new_task = Task(
+                    title=task['title'],
+                    description=task['description']
+                    # priority=task['priority'],
+                    # due_date=task['due_date']
+                )
+                db.session.add(new_task)
+                return jsonify(task.to_dict() for task in request_body), 201
+        else:
+            new_task = Task(
+                title=request_body['title'],
+                description=request_body['description']
+                # priority=request_body['priority'],
+                # due_date=request_body['due_date']
+            )
+            db.session.add(new_task)
+            db.session.commit()
+            return jsonify(new_task.to_dict()), 201
+
+@tasks_bp.route('/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = Task.query.get(task_id)
+    if not task:
+        return make_response(jsonify({'message': 'Task not found'}), 404)
+    return jsonify(task.to_dict()), 200
