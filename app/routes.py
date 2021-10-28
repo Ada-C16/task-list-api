5902a6dc1125990701eebab5c248fc5f05cb46de
Laurel S <ls60@protonmail.com>
2021-10-27 18:59:10 -0700

    Create all necessary CRUD routes for goal endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import db
 from flask import Blueprint, jsonify, make_response, request, abort
 from app.models.task import Task
+from app.models.goal import Goal
 import datetime
 
 
@@ -17,6 +18,10 @@ def confirm_valid_id(id, id_type):
 def get_task_from_id(id):
     confirm_valid_id(id, "task_id")
     return Task.query.get_or_404(id)
+
+def get_goal_from_id(id):
+    confirm_valid_id(id, "goal_id")
+    return Goal.query.get_or_404(id)
 
 # Tasks routes
 
@@ -92,3 +97,42 @@ def mark_completed_task_incomplete(task_id):
 
 
 # Goals routes
+@goals_bp.route("", methods=["POST"], strict_slashes=False)
+def create_goal():
+    request_body = request.get_json()
+    if "title" not in request_body:
+        return make_response({"details": "Invalid data"}, 400)
+    new_goal = Goal(title = request_body["title"])
+    db.session.add(new_goal)
+    db.session.commit()
+    return make_response({"goal": new_goal.to_dict()}, 201)
+
+@goals_bp.route("", methods=["GET"], strict_slashes=False)
+def get_all_goals():
+    goals = Goal.query.all()
+    goals_response = [goal.to_dict() for goal in goals]
+    return make_response(jsonify(goals_response), 200)
+
+# single goal routes
+@goals_bp.route("/<goal_id>", methods=["GET"], strict_slashes=False)
+def get_one_goal(goal_id):
+    selected_goal = get_goal_from_id(goal_id)
+    return make_response({"goal": selected_goal.to_dict()}, 200)
+
+
+@goals_bp.route("/<goal_id>", methods=["PUT"], strict_slashes=False)
+def update_goal(goal_id):
+    request_body = request.get_json()
+    selected_goal = get_goal_from_id(goal_id)
+    if "title" in request_body:
+        selected_goal.title = request_body["title"]
+    return make_response({"goal": selected_goal.to_dict()}, 200)
+
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"], strict_slashes=False)
+def delete_goal(goal_id):
+    selected_goal = get_goal_from_id(goal_id)
+    db.session.delete(selected_goal)
+    db.session.commit()
+    return make_response(
+        {"details": f'Goal {selected_goal.id} "{selected_goal.title}" successfully deleted'}, 200)
