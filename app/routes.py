bace2d4a2a9ddc213b2d1f83b62568cf38afdd68
Alie Ibarra <alieibarra@gmail.com>
2021-10-28 15:54:49 -0700

    Create POST Route

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,42 @@
-from flask import Blueprint
+from app import db
+from flask import Blueprint, request, make_response, jsonify
+from app.models.task import Task
 
+# DEFINE BLUEPRINT
+tasks_list_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+#CREATE
+@tasks_list_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task(
+        title=request_body["title"],
+        description=request_body["description"], 
+        completed_at=request_body["completed_at"]
+    )
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(f"New task {new_task.title} successfully created!", 201)
+
+#READ
+@tasks_list_bp.route("", methods=["GET"])
+def read_all_tasks():
+    tasks = Task.query.all()
+    tasks_repsonse = []
+    for task in tasks:
+        tasks.response.append(task.to_dict())
+    return jsonify(tasks_repsonse)
+
+
+#UPDATE
+@tasks_list_bp.route("", methods=["PATCH"]) 
+def update_task():
+    pass
+
+
+#DELETE
+@tasks_list_bp.route("", methods=["DELETE"]) 
+def delete_task():
+    pass
