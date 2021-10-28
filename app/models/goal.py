0256507892974dec22e440c518e621bfe3436f7f
Laurel S <ls60@protonmail.com>
2021-10-27 18:29:40 -0700

    Add to_dict method to Goal model

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,3 +5,9 @@ from app import db
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+        }
