5466d9d7e2ab82e8350dbb14b00e50c00f1a8fb9
Lux Barker <lux.barker13@gmail.com>
2021-10-29 11:03:24 -0700

    add endpoint to get all tasks by goal id

--- app/routes.py
+++ app/routes.py
@@ -183,12 +183,8 @@ def add_tasks_to_goal(goal_id):
     db.session.commit()
     return jsonify(goal.to_basic_dict()), 200
 
-# @goal_bp.route("/<goal_id>/tasks", methods=["GET"])
-# def get_tasks_by_goal(goal_id):
-#     goal = get_goal_by_id(goal_id)
 
-#     response_body = {
-#         "id": goal.id,
-#         "title": goal.title,
-#         "tasks": []
-#     }
+@goal_bp.route("/<goal_id>/tasks", methods=["GET"])
+def get_tasks_by_goal(goal_id):
+    goal = get_goal_by_id(goal_id)
+    return jsonify(goal.to_dict()), 200
