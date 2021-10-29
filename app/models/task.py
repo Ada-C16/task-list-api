787f870ab7d232c244cc429489e4931d6ec5b668
Jessica <jessica.w.roberts@gmail.com>
2021-10-29 10:39:45 -0700

    Add nullable constraints to Task class

--- app/models/task.py
+++ app/models/task.py
@@ -4,9 +4,8 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String)
-    description = db.Column(db.String)
-    is_complete = db.Column(db.Boolean)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True)
 
     def to_dict(self):
