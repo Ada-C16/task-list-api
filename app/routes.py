1efcac83f98b9fe47a80f8d40c9c953b357f3fda
Monica Cruz <mcatcruz@gmail.com>
2021-10-28 14:14:55 -1000

    Wave 1: 9 out of 12 tests passing.

--- app/routes.py
+++ app/routes.py
@@ -31,3 +31,30 @@ def handle_tasks():
                                             "description": new_task.description,
                                             "is_complete": False if new_task.completed_at is None else new_task.completed_at  
                                             }}, 201)
+
+@tasks_bp.route('/<task_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_one_task(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        return jsonify(task), 404
+    elif request.method == 'GET':
+        return {"task": { "id": task.task_id,
+                        "title": task.title,
+                        "description": task.description,
+                        "is_complete": False if task.completed_at is None else task.completed_at  
+                        }}
+    elif request.method == 'PUT':
+        updates = request.get_json()
+        task.title = updates['title']
+        task.description = updates['description']
+        
+        db.session.commit()
+        return make_response({"task": { "id": task.task_id,
+                                        "title": task.title,
+                                        "description": task.description,
+                                        "is_complete": False if task.completed_at is None else task.completed_at  
+                                        }})
+    elif request.method == 'DELETE':
+        db.session.delete(task)
+        db.session.commit()
+        return make_response({"details": f"Task {task.task_id} \"{task.title}\" successfully deleted"})
