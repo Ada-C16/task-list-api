9ac90453408c7ad04f0e4c4cef36ad1fd1ea796d
jacy <jacyyang04@gmail.com>
2021-10-29 09:01:42 -0700

    add read_all_tasks route

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, request, jsonify
+from app.models.task import Task
+from app import db
+
+# Blueprints
+task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+
+
+# Helper Functions
+
+
+# Routes
+@task_bp.route("", methods = ["GET"])
+def read_all_tasks():
+    task_list = []
+
+    tasks = Task.query.all()
+    for task in tasks:
+        task_list.append(task)
+    
+    return jsonify(task_list)
 
