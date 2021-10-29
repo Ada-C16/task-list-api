b70fac3eca93df9e25da1ad04ed77900e8a218af
tt-ht <ttht@protonmail.com>
2021-10-29 10:15:25 -0700

    CRUD for Goal model; wrote tests

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,3 +5,11 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+
+    def to_dict(self):
+        response_body = {
+            "id": self.goal_id,
+            "title": self.title
+            }
+        
+        return response_body
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
+import datetime, requests, os
 from app import db
 from app.models.task import Task
+from app.models.goal import Goal
 from flask import Blueprint, jsonify, make_response, request
-import datetime, requests, os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
 
 # creates a Task
 @tasks_bp.route("", methods=["POST"], strict_slashes=False)
@@ -123,3 +125,76 @@ def post_to_slack_task_notifications_channel(text):
         "text": text
     }
     requests.post(url, headers=headers, data=data)
+
+# creates a  Goal
+@goals_bp.route("", methods=["POST"], strict_slashes=False)
+def create_goal():
+    request_body = request.get_json()
+
+    if "title" not in request_body:
+        return make_response(
+            {"details": f"Invalid data"}, 400
+        )
+
+    new_goal = Goal(
+        title = request_body["title"]
+    )
+
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return make_response(
+        {"goal":(new_goal.to_dict())}, 201
+    )
+
+# reads all created Goals
+@goals_bp.route("", methods=["GET"], strict_slashes=False)
+def get_goals():
+    goals_response = []
+
+    goals = Goal.query.all()
+
+    for goal in goals:
+        goals_response.append(goal.to_dict())
+    return jsonify(goals_response), 200
+
+# single Task: Goal
+@goals_bp.route("/<goal_id>", methods=["GET"], strict_slashes=False)
+def get_goal(goal_id):
+    goal = Goal.query.get_or_404(goal_id)
+
+    return make_response(
+        {"goal":(goal.to_dict())}, 200
+    )
+
+# single Goal: update
+@goals_bp.route("/<goal_id>", methods=["PUT"], strict_slashes=False)
+def update_goal(goal_id):
+    goal = Goal.query.get_or_404(goal_id)
+    request_body = request.get_json()
+
+    if "title" not in request_body:
+        return make_response(
+            {"details": f"Invalid data"}, 400
+        )
+
+    goal.title = request_body["title"]
+
+    db.session.commit()
+
+    return make_response(
+        {"goal":(goal.to_dict())}, 200
+    )
+
+# single Goal: delete
+@goals_bp.route("/<goal_id>", methods=["DELETE"], strict_slashes=False)
+def delete_goal(goal_id):
+    goal = Goal.query.get_or_404(goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return make_response(
+        {"details": f"Goal {goal.goal_id} \"{goal.title}\" successfully deleted"}, 200
+    )
+
