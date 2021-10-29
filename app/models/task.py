21786bdc245b8af2b8f5683afd2da60ba1693273
Jessica <jessica.w.roberts@gmail.com>
2021-10-29 16:15:30 -0700

    Add Delete Route

--- app/models/task.py
+++ app/models/task.py
@@ -9,12 +9,13 @@ class Task(db.Model):
     completed_at = db.Column(db.DateTime, nullable=True)
 
     def to_dict(self):
-        if not self.completed_at:
-            self.completed_at = False
+        complete = False
+        if self.completed_at:
+            complete = True
 
         return {
             "id": self.task_id,
             "title": self.title,
             "description": self.description,
-            "is_complete": self.completed_at
+            "is_complete": complete
         }
