2a5e409452c61a051905322631ea8c49e5ab1837
Citlalli I Zarate <37793116+MiffyBruna@users.noreply.github.com>
2021-10-28 19:25:46 -0700

    "Task model completed"

--- app/models/task.py
+++ app/models/task.py
@@ -4,3 +4,15 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db(db.String, default= False)
+
+
+def to_dict(self):
+    return {
+        'task_id': self.task_id,
+        'title': self.title,
+        'description': self.description,
+        'completed_at': self.completed_at
+            }
