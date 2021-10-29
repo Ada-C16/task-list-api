fbfed5d3adb62b3432840a6d0ce0a8df282b8da0
Mariah Rosman <mrosman7@gmail.com>
2021-10-29 10:06:38 -0700

    registers and creates POST request Blueprint

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from app import db
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks_bp",__name__, url_prefix="/tasks")
+
+@tasks_bp.route('', methods=["POST"])
+def new_task():
+    request_body = request.get_json()
+
+    if "title" not in request_body or "description" not in request_body or "completed_at" not in request_body:
+        return {"details": "Invalid data"}, 400
+
+    new_task = Task(
+        title = request_body["title"],
+        description = request_body["description"],
+        completed_at = request_body["completed_at"]
+    )
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return new_task.to_dict(), 201
+
 
