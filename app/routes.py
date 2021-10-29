25e19ba652e3c9fa6756d345d2ed3e3c856d01be
Rebecca Z <80494343+rzuick@users.noreply.github.com>
2021-10-29 14:15:42 -0500

    created custome endpoints for mark_complete and mark_incomplete. wave 3 passes all tests

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from app import db
 from app.models.task import Task
 from app.models.goal import Goal
 from sqlalchemy import desc, asc, func
+from datetime import datetime
 
 task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
 goal_bp = Blueprint("goal_bp", __name__, url_prefix="/goals")
@@ -59,4 +60,21 @@ def handle_one_task(task_id):
         response_body = f'Task {task_id} "{one_task.title}" successfully deleted'
         return jsonify({"details": response_body}), 200
 
+@task_bp.route("/<task_id>/mark_complete", methods= ["PATCH"])
+def mark_complete_task(task_id):
+    one_task= Task.query.get(task_id)
+    if one_task is None:
+        return jsonify(one_task), 404
+    current_time = datetime.now()
+    one_task.completed_at = current_time
+    db.session.commit()
+    return jsonify({"task": one_task.task_dict()}), 200
 
+@task_bp.route("/<task_id>/mark_incomplete", methods= ["PATCH"])
+def mark_incomplete_task(task_id):
+    one_task = Task.query.get(task_id)
+    if one_task is None:
+        return jsonify(one_task), 404
+    one_task.completed_at = None
+    db.session.commit()
+    return jsonify({"task": one_task.task_dict()}), 200
