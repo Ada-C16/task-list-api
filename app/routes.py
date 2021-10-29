ec68cacbfde12ff321670fc199e63f9e42b04a95
Laurel S <ls60@protonmail.com>
2021-10-29 12:00:26 -0700

    Uncomment goal routes accident

--- app/routes.py
+++ app/routes.py
@@ -121,45 +121,45 @@ def mark_completed_task_incomplete(task_id):
 
 
 # Goals routes
-# @goals_bp.route("", methods=["POST"], strict_slashes=False)
-# def create_goal():
-#     request_body = request.get_json()
-#     if "title" not in request_body:
-#         return make_response({"details": "Invalid data"}, 400)
-#     new_goal = Goal(title = request_body["title"])
-#     db.session.add(new_goal)
-#     db.session.commit()
-#     return make_response({"goal": new_goal.to_dict()}, 201)
+@goals_bp.route("", methods=["POST"], strict_slashes=False)
+def create_goal():
+    request_body = request.get_json()
+    if "title" not in request_body:
+        return make_response({"details": "Invalid data"}, 400)
+    new_goal = Goal(title = request_body["title"])
+    db.session.add(new_goal)
+    db.session.commit()
+    return make_response({"goal": new_goal.to_dict()}, 201)
 
-# @goals_bp.route("", methods=["GET"], strict_slashes=False)
-# def get_all_goals():
-#     goals = Goal.query.all()
-#     goals_response = [goal.to_dict() for goal in goals]
-#     return make_response(jsonify(goals_response), 200)
+@goals_bp.route("", methods=["GET"], strict_slashes=False)
+def get_all_goals():
+    goals = Goal.query.all()
+    goals_response = [goal.to_dict() for goal in goals]
+    return make_response(jsonify(goals_response), 200)
 
-# # single goal routes
-# @goals_bp.route("/<goal_id>", methods=["GET"], strict_slashes=False)
-# def get_one_goal(goal_id):
-#     selected_goal = get_goal_from_id(goal_id)
-#     return make_response({"goal": selected_goal.to_dict()}, 200)
-
-
-# @goals_bp.route("/<goal_id>", methods=["PUT"], strict_slashes=False)
-# def update_goal(goal_id):
-#     request_body = request.get_json()
-#     selected_goal = get_goal_from_id(goal_id)
-#     if "title" in request_body:
-#         selected_goal.title = request_body["title"]
-#     return make_response({"goal": selected_goal.to_dict()}, 200)
+# single goal routes
+@goals_bp.route("/<goal_id>", methods=["GET"], strict_slashes=False)
+def get_one_goal(goal_id):
+    selected_goal = get_goal_from_id(goal_id)
+    return make_response({"goal": selected_goal.to_dict()}, 200)
 
 
-# @goals_bp.route("/<goal_id>", methods=["DELETE"], strict_slashes=False)
-# def delete_goal(goal_id):
-#     selected_goal = get_goal_from_id(goal_id)
-#     db.session.delete(selected_goal)
-#     db.session.commit()
-#     return make_response(
-#         {"details": f'Goal {selected_goal.id} "{selected_goal.title}" successfully deleted'}, 200)
+@goals_bp.route("/<goal_id>", methods=["PUT"], strict_slashes=False)
+def update_goal(goal_id):
+    request_body = request.get_json()
+    selected_goal = get_goal_from_id(goal_id)
+    if "title" in request_body:
+        selected_goal.title = request_body["title"]
+    return make_response({"goal": selected_goal.to_dict()}, 200)
+
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"], strict_slashes=False)
+def delete_goal(goal_id):
+    selected_goal = get_goal_from_id(goal_id)
+    db.session.delete(selected_goal)
+    db.session.commit()
+    return make_response(
+        {"details": f'Goal {selected_goal.id} "{selected_goal.title}" successfully deleted'}, 200)
 
 
 # goals and tasks routes
