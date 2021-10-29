93a909b9e56f0d3aea9fe5403c17d91b6a91d09b
Roslyn Melookaran <rmelookaran@gmail.com>
2021-10-29 10:04:11 -0700

    skipped wave 4 (for now) and moved on to wave 5. Goal functionality added, along with tests

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,11 @@ from app import db
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+
+    def to_dict(self):
+        # is_complete = False if not self.completed_at else True
+        return {
+            "id": self.goal_id,
+            "title": self.title
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
 import datetime
 from app import db
 from app.models.task import Task
+from app.models.goal import Goal
 from flask import request, Blueprint, make_response, jsonify
 from datetime import datetime
 from tests.conftest import completed_task
 
 task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+goal_bp = Blueprint("goal_bp", __name__, url_prefix="/goals")
 
 @task_bp.route("", methods=["GET", "POST"])
 def handle_tasks():
@@ -67,6 +69,43 @@ def handle_task_incomplete(task_id):
             return make_response(f"Book {task_id} not found", 404)
         task.completed_at = None
         return make_response({"task": task.to_dict()}, 200)
+
+@goal_bp.route("", methods=["GET", "POST"])
+def handle_goals():
+    goal_response = []
+    if request.method == "GET":
+        goals = Goal.query.all()
+        goal_response = [goal.to_dict() for goal in goals]
+        return jsonify(goal_response), 200
+    elif request.method == "POST":
+        request_body = request.get_json()
+        if "title" not in request_body:
+            return make_response({"details": "Invalid data"}, 400) 
+        new_goal = Goal(title=request_body["title"])
+        db.session.add(new_goal)
+        db.session.commit()
+        return make_response({"goal": new_goal.to_dict()}, 201)
+
+@goal_bp.route("/<goal_id>", methods=["GET", "DELETE", "PUT", "PATCH"])
+def handle_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+    if request.method == "GET":
+        if not goal:
+            return make_response(f"Goal {goal_id} not found", 404)
+        return {"goal": goal.to_dict()}
+    elif request.method == "DELETE":
+        if not goal:
+            return make_response("", 404)
+        db.session.delete(goal)
+        db.session.commit()
+        return {"details": f'Goal {goal.goal_id} "{goal.title}" successfully deleted'}
+    elif request.method == "PUT":
+        if not goal:
+            return make_response("", 404)
+        request_body = request.get_json()
+        goal.title = request_body["title"] if "title" in request_body else goal.title
+        return make_response({"goal": goal.to_dict()}, 200)
+
 
 
 
