5df870d7be766fa9bb88d5d19cd38a73ef2f6dcb
jacy <jacyyang04@gmail.com>
2021-10-29 08:42:40 -0700

    add task model

--- app/models/task.py
+++ app/models/task.py
@@ -4,3 +4,16 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, default=None)
+
+def to_dict(self):
+    new_dict = {
+        "id": self.id,
+        "name": self.name,
+        "description": self.description,
+        "completed_at": self.completed_at
+        }
+    
+    return new_dict
