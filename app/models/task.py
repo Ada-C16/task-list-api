bd63b8c23fd7aaa95905b3dedd02543f3813795b
Mariah Rosman <mrosman7@gmail.com>
2021-10-29 12:32:55 -0700

    adds GET method to / tasks route

--- app/models/task.py
+++ app/models/task.py
@@ -5,24 +5,24 @@ from datetime import datetime
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(64))
-    description = db.Column(db.String(64))
+    title = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(64), nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True)
 
     def to_dict(self):
         if not self.completed_at:
-            return {"task": {
-                "id": self.task_id,
-                "title": self.title,
-                "description": self.description,
-                "is_complete": False
-            }
-            }
+            return {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": False
+        }
         
-        return {"task": {
+        
+        return {
             "id": self.task_id,
             "title": self.title,
             "description": self.description,
             "is_complete": self.completed_at
-        }}
+        }
 
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, abort
 from app import db
 from app.models.task import Task
 
 tasks_bp = Blueprint("tasks_bp",__name__, url_prefix="/tasks")
 
+# add task to taks database
 @tasks_bp.route('', methods=["POST"])
 def new_task():
     request_body = request.get_json()
@@ -20,6 +21,28 @@ def new_task():
     db.session.add(new_task)
     db.session.commit()
 
-    return new_task.to_dict(), 201
+    return {"task": new_task.to_dict()}, 201
+
+# get tasks
+@tasks_bp.route('', methods=["GET"])
+def get_all_tasks():
+    tasks = Task.query.all()
+    response_tasks = []
+
+    for task in tasks:
+        response_tasks.append(task.to_dict())
+
+    return jsonify(response_tasks), 200
+
+@tasks_bp.route('/<task_id>', methods=["GET"])
+def get_one_task(task_id):
+    task = Task.query.get(task_id)
+    
+    if not task:
+        abort(404)
+
+    return {"task": task.to_dict()}, 200
+
+
 
 
