03176387e65a19bd5bcc18c38e17aa265b4d5e07
Ana Gabriele <gabbanasoares@gmail.com>
2021-10-29 19:53:05 -0700

    Change test_update from client.put() to clien.patch()

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -91,7 +91,7 @@ def test_create_task_with_none_completed_at(client):
 
 def test_update_task(client, one_task):
     # Act
-    response = client.put("/tasks/1", json={
+    response = client.patch("/tasks/1", json={
         "title": "Updated Task Title",
         "description": "Updated Test Description",
     })
@@ -116,7 +116,7 @@ def test_update_task(client, one_task):
 
 def test_update_task_not_found(client):
     # Act
-    response = client.put("/tasks/1", json={
+    response = client.patch("/tasks/1", json={
         "title": "Updated Task Title",
         "description": "Updated Test Description",
     })
