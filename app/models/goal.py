e181dc64aab14208436bb0017af153b8d71fef52
sarahstandish <z.sisyphusrocks@gmail.com>
2021-10-30 10:52:32 -0700

    add goal model

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,11 @@ from app import db
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    __tablename__ = "goals"
+
+    def to_dict(self):
+        return {
+            "id": self.goal_id,
+            "title": self.title
+        }
