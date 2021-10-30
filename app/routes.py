70f50a21dc09b84b64d4d1bf618a373863e73d11
Rhyannon <rhyannonjoy@gmail.com>
2021-10-29 22:56:54 -0600

    Passes Wave 1

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import db
-from app.models.task import Task
-from app.models.goal import Goal
+from app.models.task import Task 
 from flask import Blueprint, jsonify, make_response, request 
+from app.models.goal import Goal
 
 tasks_bp = Blueprint("tasks",__name__, url_prefix=("/tasks" ))
 
@@ -25,11 +25,12 @@ def handle_task():
     elif request.method == "POST":
         request_body = request.get_json()
         
-        if "title" not in request_body or "description" not in request_body or "completed_at" not in request_body:
-                    #return make_response("400 Bad Request",404)
-                    return{
-                        "details":"Invalid data"
-                    },400
+        if "title" not in request_body or "description" not in \
+        request_body or "completed_at" not in request_body:
+            #return make_response("400 Bad Request",404)
+            return{
+                "details":"Invalid data"
+            },400
 
 
         new_task = Task(
@@ -41,10 +42,18 @@ def handle_task():
         db.session.add(new_task)
         db.session.commit()
 
-        return make_response(f"task {new_task.title} successfully created", 201)
+        return {
+            "task":{
+                "id":new_task.task_id,
+                "title":new_task.title,
+                "description": new_task.description,
+                "is_complete":False if new_task.completed_at == None else True
+            }
+        },201
+        # return make_response(f"task {new_task.title} successfully created", 201)
 
  
-@tasks_bp.route("/<task_id>",methods=["GET","PUT"])
+@tasks_bp.route("/<task_id>",methods=["GET","PUT","DELETE"])
 def handle_task_get(task_id):
     task = Task.query.get(task_id)
     if task == None:
@@ -75,36 +84,13 @@ def handle_task_get(task_id):
                 "description":task.description,
                 "is_complete":False if task.completed_at == None else True
             }
-
-        })
+        }),200
 
     elif request.method == "DELETE":
         db.session.delete(task)
         db.session.commit()
-        return {
-                f"details":{task.description}("successfully deleted"),
-        },400
-                
-#wave 2 - asc//desc sort    
-@tasks_bp.route("/tasks?sort=asc", methods=["GET"])
-def handle_task_asc():
-    if request.method == "GET":
-        tasks = Task.query.all()
-        tasks_response = []
-        task_asc_sort = sorted(tasks_response, key=lambda d: d['title'])   
-        for task in tasks:
-                tasks_response.append([
-                {
-                "id":task.task_id,
-                "title":task.title,
-                "description":task.description,
-                "is_complete":False if task.completed_at == None else True
-                }])   
-        # task_asc_desc = sorted(tasks_response, key=itemgetter('name'), reverse=True)
-        # #task_asc_sort = sorted(tasks_response, key=lambda d: d['title'])  
+        #request_body = request.get._json()
 
-        return jsonify(task_asc_sort),200
-
-#somehow need to sort the by title in desc 
-
-#wave 3 
+        return jsonify({
+            "details": f'Task {task.task_id} "{task.title}" successfully deleted'
+            }),200
