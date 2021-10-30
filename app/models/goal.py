9088963b51a0b192ab42b59c05e627549863895a
Karina <infernormal@gmail.com>
2021-10-29 18:10:08 -0700

    wave 5 completed

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,4 +3,10 @@ from app import db
 
 
 class Goal(db.Model):
+    __tablename__ = "Goals"
     goal_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+
+    def to_dict(self):
+        return ({"goal": {"id": self.goal_id, 
+                "title": self.title}})
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask.globals import session
 from flask.wrappers import Response
 from app import db
 from app.models.task import Task
+from app.models.goal import Goal
 from flask import Blueprint, jsonify, make_response, request
 from datetime import datetime
 import os
@@ -10,6 +11,9 @@ from dotenv import load_dotenv
 import requests
 
 tasks_bp = Blueprint("tasks_bp", __name__,url_prefix="/tasks")
+goals_bp = Blueprint("goals_bp", __name__,url_prefix="/goals")
+
+#TASKS ROUTES
 
 @tasks_bp.route("",methods =["POST","GET"])
 def handle_tasks():
@@ -126,3 +130,57 @@ def mark_complete(task_id):
     
     return (task.to_dict()),200
 
+#GOALS ROUTES
+
+@goals_bp.route("",methods =["POST","GET"])
+def handle_goals():
+    if request.method == "POST":
+        request_body = request.get_json()
+        if "title" not in request_body:
+            return {
+                "details": "Invalid data"
+        },400
+        new_goal = Goal(
+            title = request_body["title"])
+
+        db.session.add(new_goal)
+        db.session.commit()
+        return (new_goal.to_dict()),201
+
+    elif request.method == "GET":
+        goals_response = []
+        goals = Goal.query.all()
+        for goal in goals:
+            goal=({"id": goal.goal_id, 
+                "title": goal.title})
+            goals_response.append(goal)
+        
+        return jsonify(goals_response)
+
+@goals_bp.route("/<goal_id>", methods = ["GET","PUT","DELETE"])
+def handle_goal(goal_id):
+    if not goal_id.isnumeric():
+        return { "Error": f"{goal_id} must be numeric."}, 404
+    goal_id = int(goal_id)
+    goal = Goal.query.get(goal_id)
+    if not goal:
+        return "None",404
+
+    if request.method == "GET":
+        return (goal.to_dict()),200
+
+    elif request.method == "PUT":
+        form_data = request.get_json()
+        goal.title = form_data["title"]
+
+        db.session.commit()
+
+        return (goal.to_dict()),200
+
+    elif request.method == "DELETE":
+        db.session.delete(goal)
+        db.session.commit()
+
+        return {
+            'details': f'Goal {goal.goal_id} "{goal.title}" successfully deleted'
+        }, 200
