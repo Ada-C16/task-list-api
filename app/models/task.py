b0280faa78d2c6b2eabc5f2bfdc93cbf369e2233
bailey <baileyholderman@gmail.com>
2021-10-31 15:25:43 -0700

    first iteration of Task model

--- app/models/task.py
+++ app/models/task.py
@@ -3,4 +3,15 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    completed_at = db.Column(db.DateTime, default=None)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_completed": True if self.completed_at else False
+        }
