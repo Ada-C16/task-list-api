38d5f77b4496a63d4fb6185399c2314f7e5de0a5
Elly Wong <elly.yywong@gmail.com>
2021-10-31 21:07:17 -0700

    passed all tests

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,4 +3,14 @@ from app import db
 
 
 class Goal(db.Model):
+    __tablename__ = "goals"
     goal_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    tasks = db.relationship("Task", backref="goal", lazy=True)
+
+
+    def convert_a_goal_to_dict(self):
+        return {
+            "id" : self.goal_id,
+            "title" : self.title,   
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,36 @@
 from app import db
 from app.models.task import Task
 from flask import Blueprint, jsonify, make_response, request
+from sqlalchemy import asc, desc
+import datetime
+from app.models.goal import Goal
+import requests, json
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 
 task_bp = Blueprint("task", __name__, url_prefix="/tasks")
 
-def helper_func_convert_a_task_to_dict(task):
-    task_dict = {
-        "id" : task.task_id,
-        "title" : task.title,
-        "description" : task.description,
-        "is_complete" : True if task.completed_at else False
-    }
+goal_bp = Blueprint("goal", __name__, url_prefix="/goals")
 
-    return task_dict
+def helper_func_convert_a_json_to_dict(json_obj, type_task_or_goal):
+    if type_task_or_goal == "task_type":
+        task_dict = {
+            "id" : json_obj.task_id,
+            # "goal_id" : json_obj.goal_id,
+            "title" : json_obj.title,
+            "description" : json_obj.description,
+            "is_complete" : True if json_obj.completed_at else False
+        }
+        return task_dict
+
+    elif type_task_or_goal == "goal_type":
+        goal_dict = {
+            "id" : json_obj.goal_id,
+            "title" : json_obj.title,   
+        }
+        return goal_dict
 
 
 @task_bp.route("", methods=["GET", "POST"])
@@ -21,11 +39,17 @@ def handle_a_task():
         all_tasks = Task.query.all()
         if not all_tasks:
             return jsonify([]), 200
-        else:
-            tasks_response = []
-            for task in all_tasks:
-                tasks_response.append(helper_func_convert_a_task_to_dict(task))
-            return jsonify(tasks_response), 200
+
+        sort_query = request.args.get("sort", default="asc")  #**
+        if sort_query == "asc":
+            all_tasks = Task.query.order_by(asc(Task.title)).all()
+        elif sort_query == "desc":
+            all_tasks = Task.query.order_by(desc(Task.title)).all()
+
+        tasks_response = []
+        for task in all_tasks:
+            tasks_response.append(task.convert_a_task_to_dict())
+        return jsonify(tasks_response), 200
 
     elif request.method == "POST":
         request_body = request.get_json()
@@ -38,9 +62,7 @@ def handle_a_task():
             db.session.add(new_task)
             db.session.commit()
 
-            helper_func_convert_a_task_to_dict(new_task)
-
-            return {"task" : helper_func_convert_a_task_to_dict(new_task)}, 201
+            return {"task" : new_task.convert_a_task_to_dict()}, 201
         except KeyError:
             return {
                 "details": "Invalid data"
@@ -51,8 +73,10 @@ def handle_a_task():
 def handle_one_task_with_id(task_id):
     task = Task.query.get_or_404(task_id)
     if request.method == "GET":
-        if task:
-            return {"task":helper_func_convert_a_task_to_dict(task)}, 200
+        if task and task.goal_id:
+            return {"task": task.convert_a_task_to_dict(task.goal_id)}, 200
+        else:
+            return {"task": task.convert_a_task_to_dict()}, 200
 
     elif request.method == "PUT":
         request_body = request.get_json()
@@ -63,8 +87,8 @@ def handle_one_task_with_id(task_id):
             db.session.add(task)
             db.session.commit()
             return {    
-                "task" : helper_func_convert_a_task_to_dict(task)
-                }, 200
+                "task" : task.convert_a_task_to_dict()
+            }
         except KeyError:
             return {
                 "message" : "require both title and description"
@@ -74,8 +98,104 @@ def handle_one_task_with_id(task_id):
         db.session.delete(task)
         db.session.commit()
         return {"details": f'Task {task.task_id} \"{task.title}\" successfully deleted'}
+
+@task_bp.route("<int:task_id>/<mark_complete_or_incomplete>", methods=["PATCH"])
+def mark_a_task_completed_with_id(task_id, mark_complete_or_incomplete):
+    task = Task.query.get_or_404(task_id)
+    if mark_complete_or_incomplete == "mark_complete": 
+        task.completed_at = datetime.date.today()
+
+        token = os.environ.get("SLACK_API_TOKEN")
+        url = "https://slack.com/api/chat.postMessage"
+        headers = {'Authorization': 'Bearer ' + token}
+        
+        response = requests.post(url, json= {"Someone just completed the task {task.title}"}, headers={'Authorization': 'Bearer ' + token})
+
+    else:   # mark_incomplete
+        task.completed_at = None
+    db.session.commit()
+    return {"task": task.convert_a_task_to_dict()}
+
+
+@goal_bp.route("", methods=["GET", "POST"])
+def handle_goals():
+    if request.method == "GET":
+        goals = Goal.query.all()
+        if not goals:
+            return jsonify([]), 200
+        else:
+            response = []
+            for goal in goals:
+                response.append(goal.convert_a_goal_to_dict())
+            return jsonify(response), 200
+
+    else:   #POST
+        request_body = request.get_json()
+        try:
+            new_goal = Goal(title=request_body["title"])
+
+            db.session.add(new_goal)
+            db.session.commit()
+
+            return {"goal" : new_goal.convert_a_goal_to_dict()}, 201
+        except KeyError:
+            return {
+                "details": "Invalid data"
+                }, 400
+
+
+@goal_bp.route("/<int:goal_id>", methods=["GET", "PUT", "DELETE"])
+def handle_a_goal_with_id(goal_id):
+    goal = Goal.query.get_or_404(goal_id)
+    if request.method == "GET":
+        return {"goal" : goal.convert_a_goal_to_dict()}
+
+    elif request.method == "PUT":
+        request_body = request.get_json()
+        try:
+            goal.title = request_body["title"]
+
+            db.session.commit()
+            return {"goal" : goal.convert_a_goal_to_dict()}, 200
+        except:
+            return make_response("missing require fields")
     
-    
+    else:   # DELETE
+        db.session.delete(goal)
+        db.session.commit()
+        return {"details": f'Goal {goal.goal_id} \"{goal.title}\" successfully deleted'}
+
+
+@goal_bp.route("/<int:goal_id>/tasks", methods=["POST", "GET"])
+def relating_a_list_of_taskIDs_to_a_goal(goal_id):
+    goal = Goal.query.get_or_404(goal_id)
+    if request.method == "GET":
+        tasks_with_specific_goal_id = Task.query.filter_by(goal_id=goal_id).all()
+        tasks_list = []
+        for task in tasks_with_specific_goal_id:
+            tasks_list.append(task.convert_a_task_to_dict(goal_id))
+
+        goal_dict = goal.convert_a_goal_to_dict()
+        goal_dict["tasks"] = tasks_list
+
+        return goal_dict
+        
+        
+
+    else: #POST   
+        request_body = request.get_json()
+        task_ids_list = request_body["task_ids"]
+        for task_id in task_ids_list:
+            task = Task.query.get_or_404(task_id)
+            task.goal_id = goal_id
+        goal.task = task_ids_list
+        
+        db.session.commit()
+        
+        return {
+            "id" : goal_id,
+            "task_ids" : task_ids_list
+        }, 200
 
 
 
