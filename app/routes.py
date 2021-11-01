366f872d4a2fd37e1ff1ceb12786eba60a69446d
Arianna Cabebe <aricabebe@gmail.com>
2021-10-31 23:05:43 -0700

    adds Task model and began creating POST route

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,17 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, request, jsonify
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@task_bp.routes("", methods=["POST"])
+def handle_tasks():
+    if request.methods == "POST":
+        request_body = request.get_json()
+        new_task = Task(
+            name= request_body["name"],
+            description= request_body["description"],
+            
+
+        )
 
