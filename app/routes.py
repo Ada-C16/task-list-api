9f2a4355c2e9fba51bb8dfcff24296ea468860a6
kaitlyngore <kaitlyngore@gmail.com>
2021-11-01 16:01:11 -0400

    created goals model, passes wave 5

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, make_response, request
 from app.models.task import Task
+from app.models.goal import Goal
 from app import db
 from sqlalchemy import desc, asc
 import datetime, requests
@@ -7,6 +8,7 @@ import os
 from dotenv import load_dotenv
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
 
 @tasks_bp.route("", methods=["GET", "POST"])
 def handle_all_tasks():
@@ -116,3 +118,76 @@ def task_incomplete(task_id):
 
         task_response = {"task": task.create_dict()}
         return jsonify(task_response), 200
+
+
+# Start Goals Routes
+
+@goals_bp.route("", methods=["GET", "POST"])
+def handle_all_goals():
+    if request.method == "POST":
+        request_body = request.get_json()
+        if "title" not in request_body:
+            error_dict = {"details": "Invalid data"}
+            return jsonify(error_dict), 400
+        
+        new_goal = Goal(title=request_body["title"])
+        
+        db.session.add(new_goal)
+        db.session.commit()
+
+        goal_response = {"goal": new_goal.create_dict()}
+        return jsonify(goal_response), 201
+
+    elif request.method == "GET":
+        name_from_url = request.args.get("name")
+        if name_from_url:
+            goals = Goal.query.filter_by(name=name_from_url).all()
+            if not goals:
+                goals = Goal.query.filter(Goal.name.contains(name_from_url))
+        # sort_query = request.args.get("sort")     
+        # if sort_query == "desc":
+        #     goals = Goal.query.order_by(desc(Goal.title))
+        # elif sort_query == "asc":
+        #     goals = Goal.query.order_by(asc(Goal.title))
+        else:
+            goals = Goal.query.all()
+            
+        goals_response = []
+        for goal in goals:
+            goals_response.append(goal.create_dict())
+        
+        if not goals_response:
+            goals = Goal.query.all()
+            for goal in goals:
+                goals_response.append(goal.create_dict())
+        
+        return jsonify(goals_response)
+
+@goals_bp.route("/<goal_id>", methods=["GET", "PUT", "DELETE"])
+def goals_by_id(goal_id):
+    goal = Goal.query.get(goal_id)
+    if not goal:
+        return jsonify(None), 404
+    
+    if request.method == "GET":
+        goal_response = {"goal": goal.create_dict()}
+        return jsonify(goal_response), 200
+    
+    elif request.method == "PUT":
+        form_data = request.get_json()
+
+        goal.title = form_data["title"]
+
+        db.session.commit()
+
+        goal_response = {"goal": goal.create_dict()}
+        return jsonify(goal_response), 200
+
+    elif request.method == "DELETE":
+        db.session.delete(goal)
+        db.session.commit()
+        
+        delete_string = f'Goal {goal.goal_id} "{goal.title}" successfully deleted'
+        delete_message = {"details": delete_string}
+        
+        return jsonify(delete_message), 200
