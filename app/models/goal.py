d2c1a967ca9f8e1183fbccb4570075f6e01108e7
sarahstandish <z.sisyphusrocks@gmail.com>
2021-11-01 15:51:35 -0700

    create relationship between task and goal models

--- app/models/goal.py
+++ app/models/goal.py
@@ -6,6 +6,7 @@ class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     __tablename__ = "goals"
+    tasks = db.relationship("Task", back_populates="goals")
 
     def to_dict(self):
         return {
--- app/models/task.py
+++ app/models/task.py
@@ -1,4 +1,5 @@
 from flask import current_app
+from sqlalchemy.orm import relation
 from app import db
 
 
@@ -8,13 +9,14 @@ class Task(db.Model):
     title = db.Column(db.String(50))
     description = db.Column(db.String(200))
     completed_at = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goals.goal_id'))
+    goals = db.relationship("Goal", back_populates="tasks")
 
     def to_dict(self):
         return {
             "id": self.task_id,
+            "goal_id": self.goal_id,
             "title": self.title,
             "description": self.description,
             "is_complete": bool(self.completed_at)
         }
-
-    
