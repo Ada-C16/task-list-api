ff541910d3cc0bcd4e698143ea9967c20e5f02fa
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-11-01 20:10:36 -0700

    create one to many relationship

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,6 +5,7 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
+    tasks = db.relationship('Task', backref='goal', lazy=True)
 
     def to_dict(self):
         return {
--- app/models/task.py
+++ app/models/task.py
@@ -5,23 +5,24 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200), nullable=False)
-    
-    # personalized attributes
-    #priority = db.Column(db.Integer, nullable=False)
-    #due_date = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=False)
 
     # attributes with defaults
     created_date = db.Column(db.DateTime, default=db.func.current_timestamp())
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    # personalized attributes
+    #priority = db.Column(db.Integer, nullable=False)
+    #due_date = db.Column(db.DateTime, nullable=True)
 
     def to_dict(self):
         return {
             'id': self.task_id,
             'title': self.title,
             'description': self.description,
-            #'completed_at': self.completed_at,
-            #'created_date': self.created_date,
             'is_complete': True if self.completed_at else False
+            #'completed_at': self.completed_at
+            #'created_date': self.created_date
         }
 
 task_schema = {
