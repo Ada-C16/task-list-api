d6a1116e57b7ebad66113886e68d0897b749e9a7
Jessica <jessica.w.roberts@gmail.com>
2021-11-02 10:46:24 -0700

    Add get one goal route

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -57,10 +57,10 @@ def read_all_goals():
         )
     return make_response(jsonify(goal_response), 200)
 
-# @task_bp.route("/<task_id>", methods=["GET"])
-# def read_one_task(task_id):
-#     task = get_task_from_id(task_id)
-#     return make_response({"task": task.to_dict()}, 200)
+@goal_bp.route("/<goal_id>", methods=["GET"])
+def read_one_goal(goal_id):
+    goal = get_goal_from_id(goal_id)
+    return make_response({"goal": goal.to_dict()}, 200)
 
 # @task_bp.route("/<task_id>", methods=["PUT"])
 # def update_task(task_id):
