6d5f7684141ca7fa37b5c6fa25d3a95bc1896e6d
roza ajema <mentoroza@yahoo.com>
2021-11-02 08:50:56 -0700

    adds slack API call under PATCH method

--- app/models/task.py
+++ app/models/task.py
@@ -4,3 +4,42 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    completed_at = db.Column(db.DateTime, nullable=True)
+    
+ 
+
+    def to_dict(self):
+        if self.completed_at == None:
+            return {
+            "id" : self.task_id,
+            "title" : self.title,
+            "description" : self.description,
+            "is_complete":False,
+            }
+        else:
+            return {
+            "id" : self.task_id,
+            "title" : self.title,
+            "description" : self.description,
+            "is_complete":True,
+            }
+
+    def to_complete(self):
+        return {
+            "id" : self.task_id,
+            "title" : self.title,
+            "description" : self.description,
+            "is_complete":True,
+            }
+    def to_incomplete(self):
+        return {
+            "id" : self.task_id,
+            "title" : self.title,
+            "description" : self.description,
+            "is_complete":False,
+            }
+
+
+      
