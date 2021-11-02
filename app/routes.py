3c076354a1bbda3db9d300caf82ee1430a94fa55
Gabe Kelemen <gabe@gabekelemen.com>
2021-11-01 22:27:39 -0400

    adds modified task and goal models and one more passing piece of wave 6

--- app/routes.py
+++ app/routes.py
@@ -229,39 +229,44 @@ def handle_single_goal(goal_id):
 
 
 # ********************* Task / Goal routes ******************************
-    
-@goals_bp.route("/<goal_id>/tasks", methods=["GET", "POST"])
-def handle_goal_tasks(goal_id):
-  goal = Goal.query.get(goal_id)
+
+
+#list of task ids to a specific goal    
+@goals_bp.route("/<goal_id>/tasks", methods=["GET"])
+def get_tasks_one_goal(goal_id):
+  goal = Goal.query.get(goal_id) #search for goal by id
   
   if goal is None:
-    return "", 404
+    return make_response("", 404)
   
-  if request.method == "GET":
-    tasks_with_goals = Task.query.filter_by(goal_id=goal_id)
+  tasks_with_goals = Task.query.filter_by(goal_id=goal_id).all() #search for tasks with that goal id
+  tasks_with_goals_list = []
     
-    tasks_with_goals = []
+  for task in tasks_with_goals:
+    tasks_with_goals_list.append({
+      "id": task.task_id, 
+      "goal_id": task.goal_id, #goal id in the task table
+      "title": task.title, 
+      "description": task.description, 
+      "is_complete": task.is_complete()})
+   
     
-    for task in tasks_with_goals:
-      tasks_with_goals.append(
-        {
-          "id": task.task_id,
-          "title": task.title,
-          "description": task.description,
-          "is_complete": task.is_complete()
-        })
     return {"id": goal.goal_id,
-          "title": goal.title,}
+          "title": goal.title,
+          "tasks": tasks_with_goals_list}, 200
+
+# goals_bp.route("/<goal_id>/tasks", methods=["POST"])
+# def handle_goal_tasks_post(goal_id):
+# task = Task.query.get(task_id)
+#     task.goal_id = goal_id
+#   goal = Goal.query.get(goal_id)
+#   request_body = request.get_json(
+#   db.session.commit()
     
-  elif request.method == "POST":
-    request_body = request.get_json()
-    
-    for task_id in request_body["task_ids"]:
-      task = Task.query.get(task_id)
-      task.goal_id = goal.goal_id
-      
-    db.session.commit()
-    
-    return
+#   return jsonify({
+#       "id": goal.goal_id,
+#       "task_ids": request_body["task_ids"]}), 200
+
+  
         
     
