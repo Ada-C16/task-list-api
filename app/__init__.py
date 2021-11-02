1ffd9588fa8d96fee22f504fdc4b654aba57ba0e
Alma <abecerrilsalas@gmail.com>
2021-11-01 21:32:58 -0700

    wave_1

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,57 @@
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+
+# db = SQLAlchemy()
+# migrate = Migrate()
+
+
+# DATABASE_CONNECTION_STRING='postgresql+psycopg2://postgres:postgres@localhost:5432/task_list_api_development'
+
+# def create_app(test_config=None):
+#     app= Flask(__name__)
+#     #db config
+#     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_STRING
+    
+#     db.init_app(app)
+#     migrate.init_app(app, db)
+#     from app.models.task import Task
+#     from app.models.goal import Goal
+#     #register the blueprint
+#     from .routes import task_list_bp
+#     app.register_blueprint(task_list_bp)
+    
+
+#     return app
+
+
+
+
+#####################below is attempt one
 from flask import Flask
+# Step 1:
+# Import and initialize SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 from dotenv import load_dotenv
 
-
+# Initialize SQLAlchemy
 db = SQLAlchemy()
 migrate = Migrate()
+DATABASE_CONNECTION_STRING='postgresql+psycopg2://postgres:postgres@localhost:5432/task_list_api_development'
 load_dotenv()
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_STRING
 
-    if test_config is None:
+    # Step 2:
+    #Configure SQLAlchemy
+    if not test_config:
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_DATABASE_URI")
     else:
@@ -22,13 +59,16 @@ def create_app(test_config=None):
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_TEST_DATABASE_URI")
 
-    # Import models here for Alembic setup
+    # Import models
     from app.models.task import Task
     from app.models.goal import Goal
-
+    # Step 3:
+    # Hook up Flask & SQLALchemy
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register Blueprints here
+    from .routes import tasks_bp
+    app.register_blueprint(tasks_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,72 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, make_response, request
 
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST", "GET"])
+def handle_tasks():
+    
+    if request.method == "POST":
+        request_body = request.get_json()
+
+        if "title" not in request_body or "description" not in request_body or "completed_at" not in request_body:
+            return make_response({"details":"Invalid data"}, 400)
+
+        new_task = Task(
+            title=request_body["title"],
+            description=request_body["description"],
+            completed_at=request_body["completed_at"]
+        )
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        
+        
+        return jsonify({"task": new_task.to_dict()}), 201
+
+    elif request.method == "GET":
+        tasks=Task.query.all()
+        tasks_response = []
+        for task in tasks:
+            tasks_response.append(task.to_dict())
+        return jsonify(tasks_response), 200
+
+
+@tasks_bp.route("/<task_id>", methods= ["GET", "PUT", "DELETE"])
+def handle_task(task_id):
+    # task_id = int(task_id)
+    task = Task.query.get(task_id)
+    
+
+    if task is None:
+        return make_response(f"Task {task_id} not found"), 404
+        # return make_response(
+        #     f"Task {task_id} not found"
+        # ), 404##############
+    if request.method == "GET":
+        
+        return jsonify({"task": task.to_dict()}), 200
+        
+    elif request.method == "PUT":
+        request_body = request.get_json()
+        if "title" not in request_body or "description" not in request_body:
+            return make_response("invalid request"), 400
+
+        task.title=request_body["title"]
+        task.description=request_body["description"]
+        # task.is_complete=request_body["is complete"]
+        # task.completed_at=request_body["completed_at"]
+
+        db.session.commit()
+        # return (task_id,jsonify({"details": task.title}), 200)
+        return jsonify({"task": task.to_dict()}),200
+        # return make_response(f"Task {task.title} successfully updated"), 200
+
+    elif request.method == "DELETE":
+        db.session.delete(task)
+        db.session.commit()
+        # return make_response(f"Task {task_id} successfully deleted"), 200
+        # return (jsonify({"details": task.title}f"successfully deleted"), 200)
+        return ({'details': f'Task {task_id} "{task.title}" successfully deleted'}), 200
