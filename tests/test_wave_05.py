7b958f611ea487e32cbb003fc4e360fad9f93f00
Ana Gabriele <gabbanasoares@gmail.com>
2021-11-01 17:24:08 -0700

    Create test cases delete() wave_5

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -1,3 +1,4 @@
+from app.models.goal import Goal
 import pytest
 
 def test_get_goals_no_saved_goals(client):
@@ -84,11 +85,11 @@ def test_update_goal(client, one_goal):
     # assertion 3 goes here
     # ---- Complete Assertions Here ----
 
-@pytest.mark.skip(reason="test to be completed by student")
+# @pytest.mark.skip(reason="test to be completed by student")
 def test_update_goal_not_found(client):
     pass
     # Act
-    # ---- Complete Act Here ----
+    response = client.get_json()
 
     # Assert
     # ---- Complete Assertions Here ----
@@ -113,18 +114,19 @@ def test_delete_goal(client, one_goal):
     response = client.get("/goals/1")
     assert response.status_code == 404
 
-@pytest.mark.skip(reason="test to be completed by student")
+# @pytest.mark.skip(reason="test to be completed by student")
 def test_delete_goal_not_found(client):
-    pass
 
     # Act
-    # ---- Complete Act Here ----
+    response = client.delete("/goals/1")
+    response_body = response.get_json()
+
 
     # Assert
-    # ---- Complete Assertions Here ----
-    # assertion 1 goes here
-    # assertion 2 goes here
-    # ---- Complete Assertions Here ----
+    assert response.status_code == 404
+    assert response_body == {"error": 404,"message": "Not found","success": False}
+
+    assert Goal.query.all() == []
 
 
 def test_create_goal_missing_title(client):
