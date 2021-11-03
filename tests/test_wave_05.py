7eabb5fe00c93305e62f248074e14af15fb9842f
Elly Wong <elly.yywong@gmail.com>
2021-11-03 16:26:57 -0700

    Deleted pass in test_wave_05.py

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -1,21 +1,17 @@
 import pytest
 
 def test_get_goals_no_saved_goals(client):
-    # Act
     response = client.get("/goals")
     response_body = response.get_json()
 
-    # Assert
     assert response.status_code == 200
     assert response_body == []
 
 
 def test_get_goals_one_saved_goal(client, one_goal):
-    # Act
     response = client.get("/goals")
     response_body = response.get_json()
 
-    # Assert
     assert response.status_code == 200
     assert len(response_body) == 1
     assert response_body == [
@@ -27,11 +23,9 @@ def test_get_goals_one_saved_goal(client, one_goal):
 
 
 def test_get_goal(client, one_goal):
-    # Act
     response = client.get("/goals/1")
     response_body = response.get_json()
 
-    # Assert
     assert response.status_code == 200
     assert "goal" in response_body
     assert response_body == {
@@ -43,25 +37,19 @@ def test_get_goal(client, one_goal):
 
 
 def test_get_goal_not_found(client):
-    pass
-    # Act
     response = client.get("/goals/1")
     response_body = response.get_json()
 
-    # Assert
-    # ---- Complete Test ----
     assert response.status_code == 404
     assert response_body == None
     # ---- Complete Test ----
 
 def test_create_goal(client):
-    # Act
     response = client.post("/goals", json={
         "title": "My New Goal"
     })
     response_body = response.get_json()
 
-    # Assert
     assert response.status_code == 201
     assert "goal" in response_body
     assert response_body == {
@@ -73,15 +61,11 @@ def test_create_goal(client):
 
 
 def test_update_goal(client, one_goal):
-    pass
-    # Act
     response = client.put("/goals/1", json={
         "title": "Updated Goal Title"
     })
 
     response_body = response.get_json()
-    # Assert
-    # ---- Complete Assertions Here ----
     assert response.status_code == 200
     assert "goal" in response_body
     assert response_body == {
@@ -91,61 +75,46 @@ def test_update_goal(client, one_goal):
             }
     }
 
-    # ---- Complete Assertions Here ----
 
-# @pytest.mark.skip(reason="test to be completed by student")
+
 def test_update_goal_not_found(client):
-    pass
-    # Act
     response = client.put("/goals/1", json={
         "title": "Updated Goal Title"
     })
     response_body = response.get_json()
 
-    # Assert
-    # ---- Complete Assertions Here ----
     assert response.status_code == 404
     assert response_body == None
-    # ---- Complete Assertions Here ----
+
 
 
 def test_delete_goal(client, one_goal):
-    # Act
     response = client.delete("/goals/1")
     response_body = response.get_json()
 
-    # Assert
     assert response.status_code == 200
     assert "details" in response_body
     assert response_body == {
         "details": 'Goal 1 "Build a habit of going outside daily" successfully deleted'
     }
 
-    # Check that the goal was deleted
     response = client.get("/goals/1")
     assert response.status_code == 404
 
 
 def test_delete_goal_not_found(client):
-    pass
-
-    # Act
     response = client.delete("/goals/1")
     response_body = response.get_json()
 
-    # Assert
-    # ---- Complete Assertions Here ----
     assert response.status_code == 404
     assert response_body == None
-    # ---- Complete Assertions Here ----
+
 
 
 def test_create_goal_missing_title(client):
-    # Act
     response = client.post("/goals", json={})
     response_body = response.get_json()
 
-    # Assert
     assert response.status_code == 400
     assert response_body == {
         "details": "Invalid data"
