7525ae8d3b40bb717da11ea1d6c2601520d3e093
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2021-11-02 23:08:58 -0700

    create to_dict instance method in task.py, create POST route to be able to add new task record

--- app/models/task.py
+++ app/models/task.py
@@ -3,11 +3,17 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True, autoincrement = True),
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable = True)
     __tablename__ = 'tasks'
 
-    
+    def to_dict(self):
+        return({
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": bool(self.completed_at)
+        })
 
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, make_response
+from app import db
+from app.models.task import Task
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods = ["POST"])
+def handle_tasks():
+    request_body = request.get_json()
+    new_task = Task(
+        title = request_body["title"],
+        description = request_body["description"],
+        completed_at = request_body["completed_at"]
+    )
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response({"task": new_task.to_dict()}, 201)
