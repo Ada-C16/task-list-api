122a759d3e4753a0475a449646ff790db7b389c8
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2021-11-03 00:12:16 -0700

    add '/<task_id>' DELETE route, handle edge some edge cases, fix completed_at line by adding conditional statement

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,10 @@ tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 def handle_tasks():
     if request.method == "POST":
         request_body = request.get_json()
+        if "title" not in request_body or "description" not in request_body or "completed_at" not in request_body:
+            return make_response(
+                {"details":"Invalid data"}, 400
+            )
         new_task = Task(
             title = request_body["title"],
             description = request_body["description"],
@@ -24,13 +28,29 @@ def handle_tasks():
             task_response.append(task.to_dict())
         return jsonify(task_response), 200
 
-@tasks_bp.route("/<task_id>", methods = ["GET", "PUT"])
+@tasks_bp.route("/<task_id>", methods = ["GET", "PUT", "DELETE"])
 def handle_task(task_id):
     task = Task.query.get(task_id)
+    if not task: 
+        return make_response("", 404)     
     if request.method == "GET":
-        if task: 
-            return make_response({"task": (task.to_dict())}, 200)
-        return make_response("", 404)
+        return make_response({"task": (task.to_dict())}, 200)    
+    elif request.method == "PUT":
+        request_body = request.get_json()
+        task.title = request_body["title"]
+        task.description = request_body["description"]
+        if "completed_at" in request_body:
+            task.completed_at = request_body["completed_at"]
+        else:
+            task.completed_at = None
+        db.session.commit()
+        return make_response({"task": task.to_dict()}, 200)
+    elif request.method == "DELETE":
+        db.session.delete(task)
+        db.session.commit()
+        return make_response({"details": f'Task {task.task_id} "{task.title}" successfully deleted'})
+
+
     
 
 
