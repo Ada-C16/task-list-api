ea55ed55824ee63a4cf66cf648c75d98d016e52a
sjolivas <sjolivas@gmail.com>
2021-11-03 17:04:53 -0500

    Created Task model and registered tasks_bp

--- app/models/task.py
+++ app/models/task.py
@@ -3,4 +3,15 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+    def task_dict(self):
+        return {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": False if self.completed_at is None else True,
+        }
