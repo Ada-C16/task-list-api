27ca50ff9b989e9f6b4620ea83443b30d2667fa7
Rebeca <muniz.movement@gmail.com>
2021-11-03 18:11:03 -0700

    wave3

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,19 @@
 from app import db
 from app.models.task import Task
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, abort, make_response
 from datetime import datetime, timezone
 
 tasks_bp = Blueprint("task_list", __name__, url_prefix="/tasks")
+
+def valid_int(number, parameter_type):
+    try:
+        int(number)
+    except:
+        abort(make_response({"error": f"{parameter_type} must be an int"})), 400
+
+def get_task_from_id(task_id):
+    valid_int(task_id, "task_id")
+    return Task.query.get_or_404(task_id, description="{task not found}")
 
 @tasks_bp.route("", methods=["POST"])
 def post_new_task():
@@ -84,33 +94,22 @@ def delete_task(task_id):
         'details': f'Task {task.task_id} "{task.title}" successfully deleted'
         }), 200
 
+# Wave 3/new endpoints updates task as complete
 @tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
-def mark_task_as_completed(task_id):
-    task = Task.query.get(task_id)
-    if task is None:
-        return jsonify(None), 404
-
-    task.completed_at = datetime.now(timezone.utc)
-
+def update_task_completion(task_id):
+    task= get_task_from_id(task_id)
+    task.is_complete=True
+    task.completed_at = datetime.now()
     db.session.commit()
-    response_body = {
-        "task": (task.to_dict())
-    }
+    return jsonify({"task": task.to_dict()}), 200
 
-    task.post_slack_message()
-
-    return jsonify(response_body), 200 
-
+# route that will mark item as incomplete
 @tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
-def mark_task_as_not_completed(task_id):
-    task = Task.query.get(task_id)
-    if task is None:
-        return jsonify(None), 404
-
-    task.completed_at = None
+def update_task_incomplete(task_id):
+    task= get_task_from_id(task_id)
+    
+    task.is_complete=False
+    task.completed_at=None
+    # db.session.add(task)
     db.session.commit()
-
-    response_body = {
-        "task": (task.to_dict())
-    }
-    return jsonify(response_body), 200 
+    return jsonify({"task": task.to_dict()}), 200 
