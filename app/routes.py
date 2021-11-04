9b82b0c747aeeceb3d4578d3d244b12096e91504
Faith <fkauwe@gmail.com>
2021-11-04 12:29:41 -0700

    final version, with comments

--- app/routes.py
+++ app/routes.py
@@ -11,21 +11,25 @@ import os
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 # assign goals_bp to the new Blueprint instance
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
+# assign slack path for slack integration
 slack_path = "https://slack.com/api/chat.postMessage"
 
 
 
 @tasks_bp.route("", methods=["POST"])
 def post_one_task():
+# request_body will be the user's input, converted to json. it will be a new record 
+# for the db, with all fields (a dict)
     request_body = request.get_json()
     if 'title' not in request_body or 'description' \
     not in request_body or 'completed_at' not in request_body:
         return make_response({"details": "Invalid data"}, 400)
     else:
+# taking infor fr request_body and converting it to new Task object    
         new_task = Task(title=request_body["title"],
                         description=request_body["description"],
                         completed_at=request_body["completed_at"])
-
+# committing changes to db
         db.session.add(new_task)
         db.session.commit()
         # using is_completed to hold Boolean value of datetime which is the data type
@@ -46,6 +50,7 @@ def working_with_all_tasks():
         tasks = Task.query.filter_by(title = title_query)
     # what part of the Task.query is actually accessing the DB?
     elif order_by_query == 'asc':
+    # getting all tasks fr the db
         tasks = Task.query.order_by(Task.title).all()
     # query_all return list of objects. loop through objects and add to empt list, task_response
     # as requested formatted JSON objects
@@ -56,7 +61,8 @@ def working_with_all_tasks():
         tasks = Task.query.order_by(Task.title).all()
     
     task_response = []
-    
+    # looping through each task, converting to requested format (dict) and adding to
+    # task_response which will be list of dicts
     for task in tasks:
         is_complete = task.completed_at is not None
         task_response.append({
@@ -96,6 +102,7 @@ def CRUD_one_task(task_id):
                                     "title": task.title,
                                     "description": task.description,
                                     "is_complete": is_complete}}, 200)
+    # patch will change just one part of the record, not the whole record
     elif request.method == "PATCH":
         form_data = request.get_json()
         if "title" in form_data:
@@ -116,18 +123,21 @@ def CRUD_one_task(task_id):
         
 @tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
 def mark_task_complete(task_id):
+# setting current datetime to time_stamp to use to mark task.completed_at
     time_stamp = datetime.now()
     task = Task.query.get(task_id) #either get Task back or None
     if task is None:
         abort(404) 
     else:
         task.completed_at = time_stamp
+# now db record shows timestamp for completed_at
     db.session.commit()
     is_complete = task.completed_at is not None
     query_params = {"channel": "slack-api-test-channel",
         "text": f"Someone just completed the task {task.title}"}
     header_param = {"Authorization": "Bearer "+ os.environ.get("slack_oauth_token")}
     slack_post_body = requests.post(slack_path, params=query_params, headers= header_param)
+# response body doesn't want to show timestamp, just True, so changing is_complete to True
     return make_response({"task": {"id": task.task_id,
                                     "title": task.title,
                                     "description": task.description,
@@ -222,9 +232,10 @@ def associate_tasks_with_goal(goal_id):
     goal = Goal.query.get(goal_id) #either get Goal back or None
 # this guard clause checks if the goal_id exists, if not, it will give 404
     if goal is None:
-# couldn't figure out another way to return no response body, researched abort
         abort(404) 
     if request.method == "POST":
+# checking all tasks, if that task_id is in the list which came from Request_body, reassign value
+# of task.goal_id (need to convert to int bc goal_id is user input and is a string)
         for task in tasks:
             if task.task_id in request_body["task_ids"]:
                 task.goal_id = int(goal_id)
