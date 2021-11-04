5461f31763a98baaf020f4d213cfd2fe8eae3b43
Katie <diazkatie@gmail.com>
2021-11-03 21:39:54 -0700

    Wave 5 passes

--- app/__init__.py
+++ app/__init__.py
@@ -30,10 +30,15 @@ def create_app(test_config=None):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    
+    from .routes.goal_routes import goal_bp
+    app.register_blueprint(goal_bp)
+
 
     # Register Blueprints here
     from .routes.task_routes import task_bp
     app.register_blueprint(task_bp)
-
-
+    
     return app
+
+ 
