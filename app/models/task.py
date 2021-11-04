57013fd65fcba5837526720b80248a32f6747ba6
Rebeca <muniz.movement@gmail.com>
2021-11-03 18:05:29 -0700

    Waves 1-3

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,7 @@
 from flask import current_app
 from app import db
+import requests
+import os
 
 
 class Task(db.Model):
@@ -7,4 +9,13 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable =True)
+    is_complete = db.Column(db.Boolean)
 
+    def to_dict(self):  
+        task_dict = {
+            "id": self.task_id, 
+            "title": self.title,
+            "description": self.description,
+            "is_complete": self.completed_at is not None
+        }
+        return task_dict
--- app/routes.py
+++ app/routes.py
@@ -1,36 +1,116 @@
 from app import db
 from app.models.task import Task
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, request
+from datetime import datetime, timezone
 
-task_list_bp = Blueprint("task_list", __name__, url_prefix="/tasks")
+tasks_bp = Blueprint("task_list", __name__, url_prefix="/tasks")
 
-@task_list_bp.route("", methods=["POST"])
-def valid_task_with_null():
+@tasks_bp.route("", methods=["POST"])
+def post_new_task():
     request_body = request.get_json()
-    valid_task = Task(title=request_body["title"],
-                    description=request_body["description"],
-                    completed_at=request_body["completed_at"])
 
-    db.session.add(valid_task)
+    if "title" not in request_body or "description" not in request_body\
+         or "completed_at" not in request_body:
+        return jsonify({
+            "details": "Invalid data"
+        }), 400
+
+    new_task = Task(title=request_body["title"],
+    description=request_body["description"],
+    completed_at=request_body["completed_at"])
+   
+    db.session.add(new_task)
     db.session.commit()
 
-    return make_response(f"task: {valid_task.title}", 201)
-   
-
-#GETS when there is at least one saved task and get this response: 
-@task_list_bp.route("/tasks", methods=["GET"])
-def getting_saved_tasks():
-    return [
-    {
-        "id": 1,
-        "title": "Example Task Title 1",
-        "description": "Example Task Description 1",
-        "is_complete": false
-    },
-    {
-        "id": 2,
-        "title": "Example Task Title 2",
-        "description": "Example Task Description 2",
-        "is_complete": false
+    response_body = {
+        "task": (new_task.to_dict())
     }
-    ]
+    return jsonify(response_body), 201
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def getting_one_task(task_id):
+    task = Task.query.get(task_id)
+
+    if task is None:
+        return jsonify(None), 404
+    
+    response_body = {
+        "task": (task.to_dict())
+    }
+    return jsonify(response_body), 200
+
+@tasks_bp.route("", methods=["GET"])
+def getting_tasks():
+    query = Task.query # Base query
+
+    # Query params, adding to query where indicated
+    sort = request.args.get("sort")
+    if sort == "asc":
+        query = query.order_by(Task.title)
+    elif sort == "desc":
+        query = query.order_by(Task.title.desc())
+    
+    query = query.all() # Final query
+    
+    # Returns jsonified list of task dicionaries
+    return jsonify([task.to_dict() for task in query])  
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def put_task(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        return jsonify(None), 404
+
+    request_body = request.get_json()
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+
+    response_body = {
+        "task": (task.to_dict())
+    }
+    return jsonify(response_body), 200
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):    
+    task = Task.query.get(task_id)
+    if task is None:
+        return jsonify(None), 404
+    
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({
+        'details': f'Task {task.task_id} "{task.title}" successfully deleted'
+        }), 200
+
+@tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
+def mark_task_as_completed(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        return jsonify(None), 404
+
+    task.completed_at = datetime.now(timezone.utc)
+
+    db.session.commit()
+    response_body = {
+        "task": (task.to_dict())
+    }
+
+    task.post_slack_message()
+
+    return jsonify(response_body), 200 
+
+@tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
+def mark_task_as_not_completed(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        return jsonify(None), 404
+
+    task.completed_at = None
+    db.session.commit()
+
+    response_body = {
+        "task": (task.to_dict())
+    }
+    return jsonify(response_body), 200 
