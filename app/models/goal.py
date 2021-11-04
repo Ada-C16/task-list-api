daa8729312963e904da2c8b9bbaea587073c9924
Mariah Rosman <mrosman7@gmail.com>
2021-11-03 18:12:07 -0700

    refactors Goal model to_dict() method

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,13 +1,22 @@
 from flask import current_app
+from sqlalchemy.orm import relationship
 from app import db
 
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+    tasks = db.relationship("Task", backref='task', lazy=True)
 
     def to_dict(self):
-        return {
-            "id": self.goal_id,
-            "title": self.title
-            }
+        if self.tasks:
+            return {
+                "id": self.goal_id,
+                "title": self.title,
+                "tasks": self.tasks
+                }
+        else:
+            return {
+                "id": self.goal_id,
+                "title": self.title
+                }
