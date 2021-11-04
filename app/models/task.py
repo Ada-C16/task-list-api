51f05f72055740a271dd077ec4372e3dee275f0f
syblades <86990570+syblades@users.noreply.github.com>
2021-11-04 10:06:20 -0700

    committed more migrations for task and goal

--- app/models/task.py
+++ app/models/task.py
@@ -1,9 +1,11 @@
-from flask import current_app
 from app import db
 
-
+#child class
+# a task belongs to one goal 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    description = db.Column(db.String)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
+
     completed_at = db.Column(db.DateTime, nullable=True)
