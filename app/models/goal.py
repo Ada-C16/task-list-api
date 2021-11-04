c38523069e7027170b7e748832c9849680627660
sjolivas <sjolivas@gmail.com>
2021-11-04 14:01:02 -0500

    Established Goal and Task relationship

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,9 +5,20 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     title = db.Column(db.String)
+    # tasks = db.relationship("Task", back_populates="goal", lazy="True")
+    tasks = db.relationship("Task", back_populates="goal")
+
 
     def goal_dict(self):
         return {
             "id": self.goal_id,
             "title": self.title
+        }
+
+
+    def goal_and_task_dict(self):
+        return{
+            "id": self.goal_id,
+            "title": self.title,
+            "tasks": [task.task_and_goal_dict() for task in self.tasks]
         }
--- app/models/task.py
+++ app/models/task.py
@@ -1,12 +1,15 @@
 from flask import current_app
 from app import db
 
-
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+    # goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"), nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"))
+    goal = db.relationship("Goal", back_populates="tasks")
+
 
     def task_dict(self):
         return {
@@ -14,4 +17,14 @@ class Task(db.Model):
             "title": self.title,
             "description": self.description,
             "is_complete": False if self.completed_at is None else True,
+        }
+
+
+    def task_and_goal_dict(self):
+        return {
+            "id":self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": False if self.completed_at is None else True,
+            "goal_id": self.goal_id
         }
