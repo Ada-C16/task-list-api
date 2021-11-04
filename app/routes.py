d4be90e33fb4e3cb60911a67c830741d299fbfa1
Michelle Bodart <michelle.bodart@gmail.com>
2021-11-04 16:06:26 -0700

    refactors routes.py to remove the id_exists functions

--- app/routes.py
+++ app/routes.py
@@ -26,18 +26,18 @@ tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 @tasks_bp.route("", methods=["POST"], strict_slashes=False)
 def post_to_tasks():
     request_body = request.get_json()
-    if is_post_task_data_valid(request_body):
+    if not is_post_task_data_valid(request_body):
+        return {"details": "Invalid data"}, 400
+    else:
         new_task = Task(title=request_body["title"], \
         description = request_body["description"], \
         completed_at = request_body["completed_at"])
-        # new_task = Task(request_body)
+        # new_task = Task(request_body) 
 
         db.session.add(new_task)
         db.session.commit()
 
-        return {"task": new_task.to_dict()}, 201
-    else:
-        return {"details": "Invalid data"}, 400
+        return {"task": new_task.to_dict()}, 201        
 
 @tasks_bp.route("", methods=["GET"], strict_slashes=False)
 def get_tasks():
@@ -57,18 +57,20 @@ def get_tasks():
 
 @tasks_bp.route('/<id>', methods=["GET"], strict_slashes=False)
 def get_task(id):
-    if not task_id_exists(id):
-        return "", 404
     task = Task.query.get(id)
+    if not task:
+        return "", 404
     return {"task": task.to_dict()}, 200
 
 @tasks_bp.route('/<id>', methods=["PUT"], strict_slashes=False)
 def put_task(id):
-    if not task_id_exists(id):
-        return "", 404
     task = Task.query.get(id)
+    if not task:
+        return "", 404
     request_body = request.get_json()
-    if is_put_task_data_valid(request_body):
+    if not is_put_task_data_valid(request_body):
+        return {"details": "Invalid data"}, 400
+    else:
         task.description = request_body['description']
         task.title = request_body['title']
         completed_at = request_body.get("completed_at")
@@ -79,23 +81,21 @@ def put_task(id):
                 return {"details": "Invalid data"}, 400
         db.session.commit()
         return {"task": task.to_dict()}, 200
-    else:
-        return {"details": "Invalid data"}, 400
-    
+
 @tasks_bp.route('/<id>', methods=["DELETE"], strict_slashes=False)
 def delete_task(id):
-    if not task_id_exists(id):
-        return "", 404
     task = Task.query.get(id)
+    if not task:
+        return "", 404
     db.session.delete(task)
     db.session.commit()
     return {"details": f"Task {str(task.task_id)} \"{str(task.title)}\" successfully deleted"}, 200
 
 @tasks_bp.route('/<id>/mark_complete', methods=["PATCH"], strict_slashes=False)
 def mark_complete(id):
-    if not task_id_exists(id):
-        return "", 404
     task = Task.query.get(id)
+    if not task:
+        return "", 404
     task.completed_at = datetime.utcnow()
     db.session.commit()
 
@@ -111,9 +111,9 @@ def mark_complete(id):
 
 @tasks_bp.route('/<id>/mark_incomplete', methods=["PATCH"], strict_slashes=False)
 def mark_incomplete(id):
-    if not task_id_exists(id):
-        return "", 404
     task = Task.query.get(id)
+    if not task:
+        return "", 404
     task.completed_at = None
     db.session.commit()
 
@@ -144,12 +144,6 @@ def is_datetime(string):
         return False
     return True
 
-def task_id_exists(id):
-    id = int(id)
-    if Task.query.get(id):
-        return True
-    return False
-
 # GOALS
 
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
@@ -173,16 +167,16 @@ def get_goals():
 
 @goals_bp.route("/<id>", methods=["GET"], strict_slashes=False)
 def get_goal(id):
-    if not goal_id_exists(id):
-        return "", 404
     goal = Goal.query.get(id)
+    if not goal:
+        return "", 404
     return {"goal": goal.to_dict()}
 
 @goals_bp.route("/<id>", methods=["PUT"], strict_slashes=False)
 def put_goal(id):
-    if not goal_id_exists(id):
-        return "", 404
     goal = Goal.query.get(id)
+    if not goal:
+        return "", 404
     request_body = request.get_json()
     goal.title = request_body["title"]
     db.session.commit()
@@ -190,18 +184,13 @@ def put_goal(id):
 
 @goals_bp.route("/<id>", methods=["DELETE"], strict_slashes=False)
 def delete_goal(id):
-    if not goal_id_exists(id):
+    goal = Goal.query.get(id)
+    if not goal:
         return "", 404
     goal = Goal.query.get(id)
     db.session.delete(goal)
     db.session.commit()
     return {"details": f"Goal {str(goal.goal_id)} \"{str(goal.title)}\" successfully deleted"}, 200
-
-def goal_id_exists(id):
-    id = int(id)
-    if Goal.query.get(id):
-        return True
-    return False
 
 def is_goal_data_valid(input):
     data_types = {"title":str}
@@ -215,7 +204,7 @@ def is_goal_data_valid(input):
 @goals_bp.route("/<goal_id>/tasks", methods=["POST"], strict_slashes=False)
 def post_goals_and_tasks(goal_id):
     goal_id = int(goal_id)
-    if not goal_id_exists(goal_id):
+    if not Goal.query.get(goal_id):
         return "", 404
     request_body = request.get_json()
     task_ids = request_body["task_ids"]
@@ -229,7 +218,7 @@ def post_goals_and_tasks(goal_id):
 @goals_bp.route("/<goal_id>/tasks", methods=["GET"], strict_slashes=False)
 def get_goals_and_tasks(goal_id):
     goal_id = int(goal_id)
-    if not goal_id_exists(goal_id):
+    if not Goal.query.get(goal_id):
         return "", 404
     tasks = [task.to_dict() for task in Task.query.filter(Task.goal_id == goal_id)]
     goal_dict = Goal.query.get(goal_id).to_dict()
