672bb7e5f9ebcf8878796fecefda5b208518395b
Megan <megan.hagar@gmail.com>
2021-11-04 11:47:59 -0700

    Attempting to add homescreen

--- app/__init__.py
+++ app/__init__.py
@@ -36,4 +36,7 @@ def create_app(test_config=None):
     from .routes import goals_bp
     app.register_blueprint(goals_bp)
 
+    from .routes import welcome_bp
+    app.register_blueprint(welcome_bp)
+
     return app
