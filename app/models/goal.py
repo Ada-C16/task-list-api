582c25373ffd9ad7904a5949389db1a1262f63f2
sjolivas <sjolivas@gmail.com>
2021-11-04 14:58:59 -0500

    Modified the Goal and Task realtionship

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,12 +1,10 @@
 from flask import current_app
 from app import db
 
-
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     title = db.Column(db.String)
-    # tasks = db.relationship("Task", back_populates="goal", lazy="True")
-    tasks = db.relationship("Task", back_populates="goal")
+    tasks = db.relationship("Task", back_populates="goal", lazy=True)
 
 
     def goal_dict(self):
