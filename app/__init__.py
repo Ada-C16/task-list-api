fa096edc506ada07fc77fc5a02e2de2c7da4ba1b
Megan <megan.hagar@gmail.com>
2021-11-04 12:01:01 -0700

    more cleanup

--- app/__init__.py
+++ app/__init__.py
@@ -36,7 +36,4 @@ def create_app(test_config=None):
     from .routes import goals_bp
     app.register_blueprint(goals_bp)
 
-    from .routes import welcome_bp
-    app.register_blueprint(welcome_bp)
-
     return app
