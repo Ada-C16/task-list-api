a6ff11b375346e2ae31e74bc5355af7cada0a952
Asli Athman <asliathman@gmail.com>
2021-11-04 13:29:47 -0400

    removing old comments

--- app/routes.py
+++ app/routes.py
@@ -33,12 +33,6 @@ def create_task():
     try:
         new_task = Task(title=request_body["title"],
                         description=request_body["description"], completed_at=request_body["completed_at"])
-        # if request_body["completed_at"]:
-        #     new_task = Task(title=request_body["title"],
-        #                     description=request_body["description"], completed_at=request_body["completed_at"])
-        # else:
-        #     new_task = Task(title=request_body["title"],
-        #                     description=request_body["description"])
         
         db.session.add(new_task)
         db.session.commit()
