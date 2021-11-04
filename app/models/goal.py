8bf366d248387e1c8e8b67b77bf88f34daa1d9f8
Mac <mackenziespeyton@gmail.com>
2021-11-03 19:44:14 -0700

    Adds goal_id to Task class and related_tasks to Goal class to initiate a one to many relationship

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,6 +5,7 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text, nullable=False)
+    related_tasks = db.relationship("Task", backref="goal")
 
     def to_dict(self):
         return({
