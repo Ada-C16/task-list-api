3f3b5cc9bf16794827c420805725514280682bd4
syblades <86990570+syblades@users.noreply.github.com>
2021-11-06 15:21:27 -0700

    light refactoring

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,5 +1,4 @@
 from app import db
-from flask import request, current_app
 
 # parent class 
 # goal can have many tasks
@@ -7,4 +6,3 @@ class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     tasks = db.relationship("Task", backref="goal", lazy=True)
-
--- app/models/task.py
+++ app/models/task.py
@@ -4,8 +4,7 @@ from app import db
 #tasks belongs to one goal 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.String(255), nullable=False)
+    title = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True)
     goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'))
-
