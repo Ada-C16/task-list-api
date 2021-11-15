39a685700a56a2d27e3e6caaf90c19c34a7b99fb
Asli Athman <asliathman@gmail.com>
2021-11-15 01:20:02 -0500

    added tod_dict method to the Goal model, migrated changes

--- app/models/goal.py
+++ app/models/goal.py
@@ -6,3 +6,14 @@ class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     tasks = db.relationship("Task", backref="goal", lazy=True)
+
+    def to_dict(self):
+        goal = {
+            "id": self.id,
+            "title": self.title
+        }
+
+        if self.tasks:
+            goal["tasks"] = self.tasks
+
+        return goal
