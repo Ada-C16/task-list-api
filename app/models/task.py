635716a62c88fbdf67c94730e2f46a86d4710354
Rebeca <muniz.movement@gmail.com>
2021-11-18 23:00:51 -0600

    wave 5 finished

--- app/models/task.py
+++ app/models/task.py
@@ -1,8 +1,8 @@
-from flask import current_app
+from flask import current_app, jsonify
 from app import db
 import requests
 import os
-
+#from app.models.goal import Goal
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +10,7 @@ class Task(db.Model):
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable =True)
     is_complete = db.Column(db.Boolean)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
 
     def to_dict(self):  
         task_dict = {
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import db
 from app.models.task import Task
+from app.models.goal import Goal
 from flask import Blueprint, jsonify, request, abort, make_response
 from datetime import datetime, timezone
 import os
@@ -120,3 +121,111 @@ def update_task_incomplete(task_id):
     # db.session.add(task)
     db.session.commit()
     return jsonify({"task": task.to_dict()}), 200 
+
+##########GOAL_ROUTES############
+#################################
+
+goals_bp = Blueprint("goals", __name__, url_prefix="/goals")    
+
+@goals_bp.route("", methods=["POST"]) #my decorator
+def create_goal(): 
+    request_body = request.get_json()
+    
+    if "title" not in request_body:
+        return {"details": "Invalid data"}, 400
+
+    new_goal = Goal(
+        title=request_body["title"]
+    )
+    
+    db.session.add(new_goal)
+    db.session.commit()
+
+    return {"goal": new_goal.to_dict()}, 201
+    
+@goals_bp.route("", methods=["GET"])
+def get_one_goal():
+    goals = Goal.query.all()
+    
+    goal_list = []
+
+    if not goals:
+        return jsonify(goal_list), 200
+    
+    for goal in goals:
+        goal_list.append(goal.to_dict())
+    
+    return jsonify(goal_list), 200
+
+@goals_bp.route("/<goal_id>/tasks", methods=["GET"])
+def read_tasks_for_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+
+    if not goal:
+        return "", 404
+
+    return {
+        "id": goal.goal_id,
+        "title": goal.title,
+        "tasks": goal.task_list()
+    }, 200
+
+@goals_bp.route("/<goal_id>", methods=["GET"])
+def get_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+
+    if goal is None:
+        return jsonify(None), 404
+    
+    response_body = {
+        "goal": (goal.to_dict())
+    }
+    
+    return jsonify(response_body), 200
+
+@goals_bp.route("/<goal_id>/tasks", methods=["POST"])
+def create_tasks_for_goal(goal_id):
+    request_body = request.get_json()
+
+    goal = Goal.query.get(goal_id)
+
+    if not goal:
+        return "", 404
+
+    for id in request_body['task_ids']:
+        goal.tasks.append(Task.query.get(id))
+
+    db.session.commit()
+
+    return {
+        "id": goal.goal_id,
+        "task_ids": request_body['task_ids']
+    }, 200
+
+@goals_bp.route("/<goal_id>", methods=["PUT", "PATCH"])
+def update_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+
+    if not goal:
+        return "", 404
+
+    request_body = request.get_json()
+
+    goal.title = request_body["title"]
+
+    db.session.commit()
+
+    return {"goal": goal.to_dict()}, 200
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = Goal.query.get(goal_id)
+
+    if not goal:
+        return "", 404
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return {"details":
+            f'Goal {goal.goal_id} "{goal.title}" successfully deleted'}, 200
