785f939f700ed20cde8fb6c60a85471c07a13f61
Maria Obregon Garcia <mobregong@gmail.com>
2021-11-20 01:32:21 -0500

    Creates task model and CRUD routes passing Wave 01 tests

--- app/models/task.py
+++ app/models/task.py
@@ -1,6 +1,20 @@
 from flask import current_app
 from app import db
+import datetime
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    # completed_at = db.Column(db.DateTime(timezone=True), default=datetime.datetime.utcnow())
+    # is_complete = db.Column(db.Boolean, nullable=False)
+    completed_at = db.Column(db.DateTime(timezone=True), nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": True if self.completed_at == True else False
+        }
