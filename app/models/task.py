88fb22f9fe317d752ea4470211d8d51bedd1efe8
Ro Peeples <rogpeeples@gmail.com>
2021-11-22 13:15:25 -0500

    passing except for 2 in wave 6

--- app/models/task.py
+++ app/models/task.py
@@ -3,4 +3,22 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable = True)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable = True)
+    #goal = db.relationship('Goal', back_populates = 'tasks', lazy = True)  unnecessary because we have backref in goal model
+
+    def to_string(self):
+        return f"{self.id}: {self.title} Description: {self.description} Completed at: {self.completed_at}"
+    
+    def combo_dict(self): 
+        return {
+            "id": self.id, 
+            "goal_id": self.goal_id,
+            "title": self.title, 
+            "description": self.description,
+            "is_complete": self.completed_at is not None
+        }
+    
