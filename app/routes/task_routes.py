79114774dfa9f364dc2dae1f6cde587a8695fd8f
Maria Obregon Garcia <mobregong@gmail.com>
2021-11-23 02:06:38 -0500

    Creates CRUD routes for goals passing all Wave 05 tests (including self-written tests)

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -27,6 +27,21 @@ def valid_task(request_body):
         abort(make_response({"details": "Invalid data"}, 400))
 # #
 
+# Create one task with error handlers
+@tasks_bp.route("", methods=["POST"], strict_slashes=False)
+def create_task():
+    request_body = request.get_json()
+    valid_task(request_body)
+    new_task = Task(
+        title=request_body["title"],
+        description=request_body["description"],
+        completed_at=request_body["completed_at"]
+    )
+    db.session.add(new_task)
+    db.session.commit()
+    response = {"task": new_task.to_dict()}
+    return make_response(response, 201)
+
 # Get all tasks
 @tasks_bp.route("", methods=["GET"], strict_slashes=False)
 def get_all_tasks():
@@ -47,28 +62,9 @@ def get_all_tasks():
 # Get one task
 @tasks_bp.route("/<task_id>", methods=["GET"], strict_slashes=False)
 def get_task(task_id):
-    # selected_task = Task.query.filter_by(task_id=task_id).one_or_none()
-    # # Task not found
-    # if selected_task is None:
-    #     abort(make_response("Not Found", 404))
     selected_task = get_task_from_id(task_id)
     response_body = {"task": selected_task.to_dict()}
     return make_response(response_body, 200)
-
-# Create one task with error handlers
-@tasks_bp.route("", methods=["POST"], strict_slashes=False)
-def create_task():
-    request_body = request.get_json()
-    valid_task(request_body)
-    new_task = Task(
-        title=request_body["title"],
-        description=request_body["description"],
-        completed_at=request_body["completed_at"]
-    )
-    db.session.add(new_task)
-    db.session.commit()
-    response = {"task": new_task.to_dict()}
-    return make_response(response, 201)
 
 # Update task
 @tasks_bp.route("/<task_id>", methods=["PUT"], strict_slashes=False)
